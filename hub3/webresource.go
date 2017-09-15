5183bdd461f5d42abd5a4b24655d422befa230eb
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2017-09-15 20:12:23 +0200

    Fixed a typo.

--- hub3/webresource.go
+++ hub3/webresource.go
@@ -6,7 +6,7 @@ import (
 	"github.com/OneOfOne/xxhash"
 )
 
-// CreateHash creates an xxhash based hash of a string
+// CreateHash creates an xxhash-based hash of a string
 func CreateHash(input string) string {
 	hash := xxhash.Checksum64([]byte(input))
 	return fmt.Sprintf("%016x", hash)
