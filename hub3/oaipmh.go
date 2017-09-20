267e465872709b05a88ca7c61cd555ee76be671b
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2017-09-20 10:42:55 +0200

    Basic scaffolding for an OAI-PMH harvester.

---
+++ hub3/oaipmh.go
@@ -0,0 +1,40 @@
+// Copyright © 2017 Delving B.V. <info@delving.eu>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hub3
+
+import (
+	"fmt"
+
+	"github.com/renevanderark/goharvest/oai"
+)
+
+// Dump a snippet of the Record metadata
+func dump(record *oai.Record) {
+	fmt.Printf("%s\n\n", record.Metadata.Body[0:500])
+}
+
+// harvestToFile demonstrates harvesting using the ListRecords verb with HarvestRecords
+func harvestToFile(baseUrl string, set string, prefix string, from string) {
+	req := &oai.Request{
+		BaseUrl:        BaseUrl,
+		Set:            set,
+		MetadataPrefix: prefix,
+		Verb:           "ListRecords",
+		From:           from,
+	}
+	// HarvestRecords passes each individual metadata record to the dump
+	// function as a Record object
+	req.HarvestRecords(dump)
+}
---
+++ hub3/oaipmh_test.go
@@ -0,0 +1,12 @@
+package hub3_test
+
+import (
+	. "bitbucket.org/delving/rapid/hub3"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Oaipmh", func() {
+
+})
