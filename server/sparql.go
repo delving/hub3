7c75bd72892e1f1c495209cbd48895b6b28f263e
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2017-09-26 21:36:59 +0200

    SPARQL proxy controller.

---
+++ server/sparql.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/labstack/gommon/log"
+
+	"bitbucket.org/delving/rapid/config"
+)
+
+// runSparqlQuery sends a SPARQL query to the SPARQL-endpoint specified in the configuration
+func runSparqlQuery(query string) (body []byte, statusCode int, err error) {
+	log.Debugf("Sparql Query: %s", query)
+	req, err := http.NewRequest("Get", config.Config.GetSparqlEndpoint(""), nil)
+	if err != nil {
+		log.Errorf("Unable to create sparql request %s", err)
+	}
+	req.Header.Set("Accept", "application/sparql-results+json")
+	rquery := req.URL.Query()
+	rquery.Set("query", query)
+	var netClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+	resp, err := netClient.Do(req)
+	if err != nil {
+		log.Errorf("Error in sparql query: %s", err)
+	}
+	body, err = ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Errorf("Unable to read the response body with error: %s", err)
+	}
+	statusCode = resp.StatusCode
+	return
+}
