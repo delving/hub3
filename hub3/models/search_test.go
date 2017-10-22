a5a239d81b290f787ab8d550695fad5a78e40c79
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2017-10-22 21:30:39 +0200

    Added scaffolding for search model ginkgo tests.

---
+++ hub3/models/search_test.go
@@ -0,0 +1,12 @@
+package models_test
+
+import (
+	. "bitbucket.org/delving/rapid/hub3/models"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Search", func() {
+
+})
