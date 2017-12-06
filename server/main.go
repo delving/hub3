26045b05f7fb8fa67970cbbbf7df74d3a58fbee1
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2017-12-06 14:05:22 +0100

    Setup default negroni middleware.

--- server/main.go
+++ server/main.go
@@ -8,36 +8,54 @@ import (
 
 	"github.com/go-chi/chi"
 	mw "github.com/go-chi/chi/middleware"
-	"github.com/go-chi/cors"
 	"github.com/labstack/gommon/log"
+	"github.com/urfave/negroni"
 )
 
 // Start starts a graceful webserver process.
 func Start() {
-	// Setup
-	r := chi.NewRouter()
-	r.Use(mw.Logger)
-	r.Use(mw.Recoverer)
-	r.Use(mw.StripSlashes)
 
-	cors := cors.New(cors.Options{
-		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	})
-	r.Use(cors.Handler)
+	// Negroni middleware manager
+	n := negroni.New()
+
+	// recovery
+	recovery := negroni.NewRecovery()
+	recovery.Formatter = &negroni.HTMLPanicFormatter{}
+	n.Use(recovery)
+
+	// logger
+	l := negroni.NewLogger()
+	n.Use(l)
+
+	// setup fileserver for public directory
+	//n.Use(negroni.NewStatic(http.Dir("public")))
+
+	// Setup Router
+	r := chi.NewRouter()
+	r.Use(mw.StripSlashes)
+	n.UseHandler(r)
+
+	//cors := cors.New(cors.Options{
+	//// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+	//AllowedOrigins: []string{"*"},
+	//// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+	//AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	//AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	//ExposedHeaders:   []string{"Link"},
+	//AllowCredentials: true,
+	//MaxAge:           300, // Maximum value not ignored by any of major browsers
+	//})
+	//r.Use(cors.Handler)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("You are rocking rapid!"))
 	})
 
+	// serve documentation
+	//r.Get("/documenation", http.FileServer(http.Dir("public")))
+
 	log.Infof("Using port: %d", Config.Port)
-	http.ListenAndServe(fmt.Sprintf(":%d", Config.Port), r)
+	http.ListenAndServe(fmt.Sprintf(":%d", Config.Port), n)
 
 	//// Admin group
 	//a := e.Group("/admin")
