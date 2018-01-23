14ea26c4ee5413ec0989bb09850240e88e3c3395
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2018-01-23 11:34:58 +0100

    Initial implementation of the search api.

--- server/search.go
+++ server/search.go
@@ -15,10 +15,18 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
+	log "log"
 	"net/http"
+	"strings"
 
+	"bitbucket.org/delving/rapid/config"
+	"bitbucket.org/delving/rapid/hub3/index"
+	"bitbucket.org/delving/rapid/hub3/models"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
+	elastic "gopkg.in/olivere/elastic.v5"
 )
 
 // SearchResource is a struct for the Search routes
@@ -28,10 +36,111 @@ type SearchResource struct{}
 func (rs SearchResource) Routes() chi.Router {
 	r := chi.NewRouter()
 
-	r.Get("/v2", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/v2", getSearchResult)
+	r.Get("/v2/{id}", func(w http.ResponseWriter, r *http.Request) {
+		getSearchRecord(w, r)
+		render.PlainText(w, r, `{"status": "not enabled"}`)
+		return
+	})
+	r.Get("/v1", func(w http.ResponseWriter, r *http.Request) {
+		render.PlainText(w, r, `{"status": "not enabled"}`)
+		return
+	})
+	r.Get("/v1/{id}", func(w http.ResponseWriter, r *http.Request) {
 		render.PlainText(w, r, `{"status": "not enabled"}`)
 		return
 	})
 
 	return r
+}
+
+func getSearchRecord(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	res, err := index.ESClient().Get().
+		Index(config.Config.ElasticSearch.IndexName).
+		Type("rdfrecord").
+		Id(id).
+		Do(ctx)
+	if err != nil {
+		log.Println("Unable to get search result.")
+		log.Println(err)
+		return
+	}
+	if res == nil {
+		log.Printf("expected response != nil; got: %v", res)
+		return
+	}
+	if !res.Found {
+		log.Printf("%s was not found", id)
+		return
+	}
+
+	record, err := decodeRFDRecord(res.Source)
+	if err != nil {
+		fmt.Printf("Unable to decode RDFRecord: %#v", res.Source)
+		return
+	}
+	render.JSON(w, r, record)
+}
+
+func getSearchResult(w http.ResponseWriter, r *http.Request) {
+	s := index.ESClient().Search().
+		Index(config.Config.ElasticSearch.IndexName).
+		Type("rdfrecord").
+		Size(20)
+	rawQuery := r.FormValue("q")
+	fmt.Println("query: ", rawQuery)
+	if rawQuery != "" {
+		rawQuery = strings.Replace(rawQuery, "delving_spec:", "spec:", 1)
+		s = s.Query(elastic.NewQueryStringQuery(rawQuery))
+	} else {
+		s = s.Query(elastic.NewMatchAllQuery())
+	}
+	res, err := s.Do(ctx)
+	if err != nil {
+		log.Println("Unable to get search result.")
+		log.Println(err)
+		return
+	}
+	if res == nil {
+		log.Printf("expected response != nil; got: %v", res)
+		return
+	}
+
+	fmt.Printf("%#v\n", res)
+	fmt.Printf("%#v\n", res.TotalHits())
+	records, err := decodeRDFRecords(res)
+	if err != nil {
+		log.Printf("Unable to decode records")
+		return
+	}
+	render.JSON(w, r, records)
+	return
+}
+
+func decodeRFDRecord(hit *json.RawMessage) (*models.RDFRecord, error) {
+	r := new(models.RDFRecord)
+	if err := json.Unmarshal(*hit, r); err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+// decodeRDFRecords takes a search result and deserializes the records
+func decodeRDFRecords(res *elastic.SearchResult) ([]*models.RDFRecord, error) {
+	if res == nil || res.TotalHits() == 0 {
+		return nil, nil
+	}
+
+	var records []*models.RDFRecord
+	for _, hit := range res.Hits.Hits {
+		r, err := decodeRFDRecord(hit.Source)
+		if err != nil {
+			return nil, err
+		}
+		// TODO Add Score here, e.g.:
+		// film.Score = *hit.Score
+		records = append(records, r)
+	}
+	return records, nil
 }
