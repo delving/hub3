321e7761525f2694744e0e46117dd1e8f3e0a939
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2018-06-23 00:18:46 +0200

    Update packagename for rapidctl.

--- rapidctl/main.go
+++ rapidctl/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package rapidctl
 
 import "github.com/delving/rapid-saas/rapidctl/cmd"
 
