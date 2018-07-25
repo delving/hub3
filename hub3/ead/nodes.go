99d6b6cdde27aff8dfae2055b5f15fd92f328ce5
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2018-07-25 12:03:49 +0200

    EAD Node can now be converted to indexable FragmentGraph.

--- hub3/ead/nodes.go
+++ hub3/ead/nodes.go
@@ -1,16 +1,228 @@
 package ead
 
-import "github.com/olivere/elastic"
+import (
+	"fmt"
 
-type NodeBuilder struct {
-	fragments chan<- *elastic.BulkIndexRequest
-	records   chan<- *elastic.BulkIndexRequest
-	spec      string
-	orgID     string
-	//r = elastic.NewBulkIndexRequest().
-	//Index(c.Config.ElasticSearch.IndexName).
-	//Type(fragments.DocType).
-	//RetryOnConflict(3).
-	//Id(action.HubID).
-	//Doc(fb.Doc())
+	"github.com/delving/rapid-saas/config"
+	"github.com/delving/rapid-saas/hub3/fragments"
+	r "github.com/kiivihal/rdf2go"
+)
+
+const FragmentGraphDocType = "ead"
+
+func newSubject(cfg *NodeConfig, id string) string {
+	return fmt.Sprintf("%s/archive/%s/%s", config.Config.RDF.BaseURL, cfg.Spec, id)
 }
+
+// FragmentGraph returns the archival node as a FragmentGraph
+func (n *Node) FragmentGraph(cfg *NodeConfig) (*fragments.FragmentGraph, error) {
+	rm := fragments.NewEmptyResourceMap()
+	id := n.GetHeader().GetInventoryNumber()
+	subject := n.GetHeader().GetSubject(cfg)
+	header := &fragments.Header{
+		OrgID:         cfg.OrgID,
+		Spec:          cfg.Spec,
+		Revision:      cfg.Revision,
+		HubID:         fmt.Sprintf("%s_%s_%s", cfg.OrgID, cfg.Spec, id),
+		DocType:       FragmentGraphDocType,
+		EntryURI:      subject,
+		NamedGraphURI: fmt.Sprintf("%s/graph", subject),
+		Modified:      fragments.NowInMillis(),
+		Tags:          []string{"ead"},
+	}
+
+	for idx, t := range n.Triples(subject, cfg) {
+		if err := rm.AppendOrderedTriple(t, false, idx); err != nil {
+			return nil, err
+		}
+	}
+
+	fg := fragments.NewFragmentGraph()
+	fg.Meta = header
+	fg.SetResources(rm)
+	return fg, nil
+}
+
+func (ni *NodeID) Triples(referrer r.Term, cfg *NodeConfig) []*r.Triple {
+	s := r.NewAnonNode()
+	triples := []*r.Triple{
+		r.NewTriple(
+			referrer,
+			r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", "unitid")),
+			s,
+		),
+		r.NewTriple(
+			s,
+			r.NewResource(fragments.RDFType),
+			r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", "Unitid")),
+		),
+	}
+
+	t := func(s r.Term, p, o string, oType convert) {
+		t := addNonEmptyTriple(s, p, o, oType)
+		if t != nil {
+			triples = append(triples, t)
+		}
+		return
+	}
+	t(s, "typeID", ni.GetTypeID(), r.NewLiteral)
+	t(s, "type", ni.GetType(), r.NewLiteral)
+	t(s, "audience", ni.GetAudience(), r.NewLiteral)
+	t(s, "identifier", ni.GetID(), r.NewLiteral)
+	return triples
+}
+
+// GetSubject creates subject URI for the parent Node
+// the header itself is an anonymous BlankNode
+func (h *Header) GetSubject(cfg *NodeConfig) string {
+	id := h.GetInventoryNumber()
+	return newSubject(cfg, id)
+}
+
+// Triples converts the EAD Did to RDF triples
+func (h *Header) Triples(subject string, cfg *NodeConfig) []*r.Triple {
+
+	s := r.NewAnonNode()
+	triples := []*r.Triple{
+		r.NewTriple(
+			r.NewResource(subject),
+			r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", "hasDid")),
+			s,
+		),
+		r.NewTriple(
+			s,
+			r.NewResource(fragments.RDFType),
+			r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", "Did")),
+		),
+	}
+	t := func(s r.Term, p, o string, oType convert) {
+		t := addNonEmptyTriple(s, p, o, oType)
+		if t != nil {
+			triples = append(triples, t)
+		}
+		return
+	}
+
+	t(s, "idUnittype", h.GetType(), r.NewLiteral)
+	t(s, "idDateAsLabel", fmt.Sprintf("%t", h.GetDateAsLabel()), r.NewLiteral)
+	t(s, "idInventorynr", h.GetInventoryNumber(), r.NewLiteral)
+	t(s, "physdesc", h.GetPhysdesc(), r.NewLiteral)
+
+	for _, label := range h.GetLabel() {
+		t(s, "idUnittitle", label, r.NewLiteral)
+	}
+	for _, nodeID := range h.GetID() {
+		triples = append(triples, nodeID.Triples(s, cfg)...)
+	}
+
+	return triples
+}
+
+// Triples returns all the triples for a NodeDate
+func (nd *NodeDate) Triples(referrer r.Term, cfg *NodeConfig) []*r.Triple {
+	s := r.NewAnonNode()
+	triples := []*r.Triple{
+		r.NewTriple(
+			referrer,
+			r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", "unitdate")),
+			s,
+		),
+		r.NewTriple(
+			s,
+			r.NewResource(fragments.RDFType),
+			r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", "Unitdate")),
+		),
+	}
+
+	t := func(s r.Term, p, o string, oType convert) {
+		t := addNonEmptyTriple(s, p, o, oType)
+		if t != nil {
+			triples = append(triples, t)
+		}
+		return
+	}
+
+	t(s, "dateCalendar", nd.GetCalendar(), r.NewLiteral)
+	t(s, "dateEra", nd.GetEra(), r.NewLiteral)
+	t(s, "dateNormal", nd.GetNormal(), r.NewLiteral)
+	t(s, "dateType", nd.GetType(), r.NewLiteral)
+	return triples
+}
+
+type convert func(string) r.Term
+
+func addNonEmptyTriple(s r.Term, p, o string, oType convert) *r.Triple {
+	if o == "" {
+		return nil
+	}
+	return r.NewTriple(
+		s,
+		r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", p)),
+		oType(o),
+	)
+}
+
+// Triples returns a list of triples created from an Archive Node
+// Nested elements are linked as object references
+func (n *Node) Triples(subject string, cfg *NodeConfig) []*r.Triple {
+	s := r.NewResource(subject)
+	triples := []*r.Triple{
+		r.NewTriple(
+			s,
+			r.NewResource(fragments.RDFType),
+			r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", "Clevel")),
+		),
+		r.NewTriple(
+			s,
+			r.NewResource("http://www.w3.org/2000/01/rdf-schema#label"),
+			r.NewLiteral(n.GetHeader().GetTreeLabel()),
+		),
+	}
+	t := func(s r.Term, p, o string, oType convert) {
+		t := addNonEmptyTriple(s, p, o, oType)
+		if t != nil {
+			triples = append(triples, t)
+		}
+		return
+	}
+
+	t(s, "cLevel", n.GetCTag(), r.NewLiteral)
+	t(s, "cType", n.GetType(), r.NewLiteral)
+	t(s, "cSubtype", n.GetSubType(), r.NewLiteral)
+	t(s, "scopecontent", n.GetHTML(), r.NewLiteral)
+
+	triples = append(triples, n.GetHeader().Triples(subject, cfg)...)
+
+	parentSubject := s
+	for i := len(n.ParentIDs) - 1; i >= 0; i-- {
+		objectSubject := r.NewResource(newSubject(cfg, n.ParentIDs[i]))
+		parent := r.NewTriple(
+			parentSubject,
+			r.NewResource(fmt.Sprintf("http://archief.nl/def/ead/%s", "hasParent")),
+			objectSubject,
+		)
+
+		parentSubject = objectSubject
+		label, ok := cfg.labels[n.ParentIDs[i]]
+		if ok {
+			objectLabel := r.NewTriple(
+				parentSubject,
+				r.NewResource("http://www.w3.org/2000/01/rdf-schema#label"),
+				r.NewLiteral(label),
+			)
+			triples = append(triples, parent, objectLabel)
+			continue
+		}
+		triples = append(triples, parent)
+	}
+
+	return triples
+}
+
+// store recursive function on nodelist for fragments and fragment graph
+
+// create fragmentGraph
+
+// node to triples
+
+// create subject URI function: base, tentant, spec, inventoryID
--- hub3/ead/nodes_test.go
+++ hub3/ead/nodes_test.go
@@ -1,1 +1,218 @@
-package ead
+package ead_test
+
+import (
+	"context"
+
+	r "github.com/kiivihal/rdf2go"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/delving/rapid-saas/hub3/ead"
+	"github.com/delving/rapid-saas/hub3/fragments"
+)
+
+var _ = Describe("Nodes", func() {
+
+	Describe("converting to RDF", func() {
+
+		Context("from a header", func() {
+			dsc := new(Cdsc)
+			err := parseUtil(dsc, "ead.1.xml")
+			var nl *NodeList
+			cfg := NewNodeConfig(context.Background())
+			cfg.OrgID = "test"
+			cfg.Spec = "test-spec"
+			cfg.Revision = int32(38)
+			var h *fragments.Header
+
+			It("should not throw an error", func() {
+				Expect(err).ToNot(HaveOccurred())
+				nl, _, err = dsc.NewNodeList(cfg)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nl).ToNot(BeNil())
+				fg, _ := nl.Nodes[0].FragmentGraph(cfg)
+				h = fg.Meta
+				Expect(h).ToNot(BeNil())
+			})
+
+			It("should have an OrgID", func() {
+				Expect(h.GetOrgID()).To(Equal("test"))
+			})
+
+			It("should have a spec", func() {
+				Expect(h.GetSpec()).To(Equal("test-spec"))
+			})
+
+			It("should have the right revision", func() {
+				Expect(h.GetRevision()).To(Equal(int32(38)))
+			})
+
+			It("should have a hubID", func() {
+				Expect(h.GetHubID()).To(Equal("test_test-spec_A"))
+			})
+
+			It("should have an EAD docType", func() {
+				Expect(h.GetDocType()).To(Equal("ead"))
+			})
+
+			It("should set a modified time", func() {
+				Expect(h.GetModified()).ToNot(BeZero())
+			})
+
+			It("should set the entryURI", func() {
+				Expect(h.GetEntryURI()).ToNot(BeEmpty())
+				Expect(h.GetEntryURI()).To(Equal("http://data.rapid.org/archive/test-spec/A"))
+			})
+
+			It("should have a NamedGraphURI", func() {
+				Expect(h.GetNamedGraphURI()).To(HavePrefix(h.GetEntryURI()))
+				Expect(h.GetNamedGraphURI()).To(HaveSuffix("/graph"))
+			})
+		})
+
+		Context("from a single cLevel node", func() {
+			dsc := new(Cdsc)
+			err := parseUtil(dsc, "ead.1.xml")
+			var nl *NodeList
+			cfg := NewNodeConfig(context.Background())
+			cfg.OrgID = "test"
+			cfg.Spec = "test_spec"
+			cfg.Revision = int32(38)
+
+			It("should not throw an error", func() {
+				Expect(err).ToNot(HaveOccurred())
+				nl, _, err = dsc.NewNodeList(cfg)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nl).ToNot(BeNil())
+			})
+
+			It("should convert only the main body to RDF", func() {
+				node := nl.Nodes[0]
+				Expect(node.GetType()).To(Equal("series"))
+				fr, err := node.FragmentGraph(cfg)
+				Expect(err).ToNot(HaveOccurred())
+				s := fr.GetAboutURI()
+				Expect(s).To(Equal("http://data.rapid.org/archive/test_spec/A"))
+			})
+
+			It("should set the meta header", func() {
+				node := nl.Nodes[0]
+				fr, err := node.FragmentGraph(cfg)
+				Expect(err).ToNot(HaveOccurred())
+				h := fr.Meta
+				Expect(h).ToNot(BeNil())
+			})
+
+			It("should have resources", func() {
+				node := nl.Nodes[0]
+				fr, err := node.FragmentGraph(cfg)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(fr).ToNot(BeNil())
+				// TODO enable later again
+				//Expect(fr.Resources).ToNot(BeNil())
+				//Expect(fr.Resources).To(HaveLen(1))
+			})
+
+		})
+
+		Context("when creating triples with parents", func() {
+			c03 := new(Cc03)
+			err := parseUtil(c03, "ead.4.xml")
+			cfg := NewNodeConfig(context.Background())
+			cfg.AddLabel("c1", "c1 label")
+			cfg.AddLabel("c2", "c2 label")
+			cfg.OrgID = "test"
+			cfg.Spec = "test_spec"
+			cfg.Revision = int32(38)
+			var node *Node
+
+			It("it should not throw an error", func() {
+				parentIDs := []string{"c1", "c2"}
+				node, err = NewNode(c03, parentIDs, cfg)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(node).ToNot(BeNil())
+			})
+
+			It("should have parentIDS", func() {
+				s := "urn:123"
+				triples := node.Triples(s, cfg)
+				Expect(triples).ToNot(BeEmpty())
+				Expect(triples).To(HaveLen(25))
+			})
+
+		})
+
+		Context("when creating triples from a Header", func() {
+			c03 := new(Cc03)
+			err := parseUtil(c03, "ead.4.xml")
+			cfg := NewNodeConfig(context.Background())
+			cfg.OrgID = "test"
+			cfg.Spec = "test_spec"
+			cfg.Revision = int32(38)
+			var h *Header
+
+			It("it should not throw an error", func() {
+				Expect(err).ToNot(HaveOccurred())
+				parentIDs := []string{"c1", "c2"}
+				node, err := NewNode(c03, parentIDs, cfg)
+				Expect(err).ToNot(HaveOccurred())
+
+				h = node.GetHeader()
+				Expect(h).ToNot(BeNil())
+			})
+
+			It("should generate a subject", func() {
+				s := h.GetSubject(cfg)
+				Expect(s).ToNot(BeEmpty())
+			})
+
+			Context("check unit date", func() {
+				var date *NodeDate
+
+				It("should have a date", func() {
+					dates := h.GetDate()
+					Expect(dates).ToNot(BeEmpty())
+					Expect(dates).To(HaveLen(1))
+					date = dates[0]
+					Expect(date).ToNot(BeNil())
+				})
+
+				It("should have triples", func() {
+					s := r.NewResource(h.GetSubject(cfg))
+					triples := date.Triples(s, cfg)
+					Expect(triples).ToNot(BeEmpty())
+					Expect(triples).To(HaveLen(6))
+				})
+			})
+
+			Context("check unitIDs", func() {
+				var id *NodeID
+
+				It("should have unitIDs", func() {
+					ids := h.GetID()
+					Expect(ids).ToNot(BeEmpty())
+					Expect(ids).To(HaveLen(2))
+					id = ids[0]
+					Expect(id).ToNot(BeNil())
+				})
+
+				It("should have an Triples", func() {
+					s := r.NewResource(h.GetSubject(cfg))
+					triples := id.Triples(s, cfg)
+					Expect(triples).ToNot(BeEmpty())
+					Expect(triples).To(HaveLen(5))
+				})
+
+			})
+
+			It("should have a list of triples", func() {
+				s := h.GetSubject(cfg)
+				triples := h.Triples(s, cfg)
+				Expect(triples).ToNot(BeEmpty())
+				Expect(triples).To(HaveLen(16))
+			})
+
+		})
+	})
+
+})
