f625139a54e960d4841ecbdda1882547e1181bd4
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2019-03-25 23:16:39 +0100

    pkg/oaipmh: added handler funcs to oaipmh package.

--- pkg/oaipmh/handler.go
+++ pkg/oaipmh/handler.go
@@ -15,8 +15,40 @@
 package harvesting
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/render"
 	"github.com/kiivihal/goharvest/oai"
 )
+
+// bindPMHRequest the query parameters to the OAI-Request
+func bindPMHRequest(r *http.Request) oai.Request {
+	baseURL := fmt.Sprintf("http://%s%s", r.Host, r.URL.Path)
+	q := r.URL.Query()
+	req := oai.Request{
+		Verb:            q.Get("verb"),
+		MetadataPrefix:  q.Get("metadataPrefix"),
+		Set:             q.Get("set"),
+		From:            q.Get("from"),
+		Until:           q.Get("until"),
+		Identifier:      q.Get("identifier"),
+		ResumptionToken: q.Get("resumptionToken"),
+		BaseURL:         baseURL,
+	}
+	return req
+}
+
+// oaiPmhEndpoint processed OAI-PMH request and returns the results
+//
+// example
+//		r.Get("/api/oai-pmh", oaiPmhEndpoint)
+func oaiPmhEndpoint(w http.ResponseWriter, r *http.Request) {
+	req := bindPMHRequest(r)
+	//log.Println(req)
+	resp := ProcessVerb(&req)
+	render.XML(w, r, resp)
+}
 
 // ProcessVerb processes different OAI-PMH verbs
 func ProcessVerb(r *oai.Request) interface{} {
