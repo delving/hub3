3c92afef79ffea781b7dd66d320083110551dd7d
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2019-04-12 23:10:54 +0200

    hub3/fragments: support lucene QueryString queries for tree.ByLabel queries. Also only add minimum should match when query is not an advanced query.

--- hub3/fragments/resource.go
+++ hub3/fragments/resource.go
@@ -116,8 +116,11 @@ func (tq *TreeQuery) GetPreviousScrollIDs(cLevel string, sr *SearchRequest, page
 
 	matchSuffix := fmt.Sprintf("_%s", strings.TrimLeft(cLevel, "@"))
 
-	q := elastic.NewMatchQuery("tree.label", sr.Tree.GetLabel())
-	q = q.MinimumShouldMatch(c.Config.ElasticSearch.MinimumShouldMatch)
+	q := elastic.NewQueryStringQuery(sr.Tree.GetLabel())
+	q = q.DefaultField("tree.label")
+	if !isAdvancedSearch(sr.Tree.GetLabel()) {
+		q = q.MinimumShouldMatch(c.Config.ElasticSearch.MinimumShouldMatch)
+	}
 	query = query.Must(q)
 	query = query.Must(elastic.NewTermQuery(c.Config.ElasticSearch.SpecKey, tq.Spec))
 
