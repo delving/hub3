5349d594ce8737f2b70b6b86078821b04f1d1689
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2020-03-12 07:25:48 +0100

    Single document full index support
    Using a search.QueryTerm a full text index can be queried. At the moment
    it supports all search options except fielded search since the single
    document does only contain text.

---
+++ ikuzo/storage/x/memory/index_full_text.go
@@ -0,0 +1,412 @@
+package memory
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/delving/hub3/ikuzo/service/x/search"
+)
+
+var (
+	ErrSearchNoMatch = errors.New("the search query does not match the index")
+)
+
+type termVector struct {
+	position map[int]bool
+	split    bool // replace with elasticsearch term for synonym in same position
+}
+
+func newTermVector() *termVector {
+	return &termVector{
+		position: make(map[int]bool),
+	}
+}
+
+func (tv *termVector) size() int {
+	return len(tv.position)
+}
+
+// TestIndex is a single document full-text index.
+// This means that all data you append to it will have its position incremented
+// and appends to the known state. It is not replaced. To reset the index to
+// an empty state you have to call the reset method.
+type TextIndex struct {
+	terms map[string]*termVector
+	a     search.Analyzer
+}
+
+func NewTextIndex() *TextIndex {
+	return &TextIndex{
+		terms: make(map[string]*termVector),
+	}
+}
+
+// appendBytes extract words from bytes and updates the TextIndex.
+func (ti *TextIndex) appendBytes(b []byte) error {
+	words := bytes.Fields(b)
+	for idx, word := range words {
+		err := ti.addTerm(string(word), idx)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// appendBytes extract words from bytes and updates the TextIndex.
+func (ti *TextIndex) appendString(text string) error {
+	words := strings.Fields(text)
+	for idx, word := range words {
+		err := ti.addTerm(word, idx)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (ti *TextIndex) reset() {
+	ti.terms = make(map[string]*termVector)
+}
+
+func (ti *TextIndex) size() int {
+	return len(ti.terms)
+}
+
+func (ti *TextIndex) setTermVector(term string, pos int, split bool) {
+	tv, ok := ti.terms[term]
+	if !ok {
+		tv = newTermVector()
+		ti.terms[term] = tv
+		tv.split = split
+	}
+
+	tv.position[pos] = true
+}
+
+func (ti *TextIndex) addTerm(word string, pos int) error {
+	analyzedTerm := ti.a.Transform(word)
+
+	if analyzedTerm == "" {
+		return fmt.Errorf("cannot index empty string")
+	}
+
+	ti.setTermVector(analyzedTerm, pos, false)
+
+	if strings.Contains(analyzedTerm, "-") {
+		for _, p := range strings.Split(analyzedTerm, "-") {
+			ti.setTermVector(p, pos, true)
+		}
+	}
+
+	return nil
+}
+
+type SearchHits struct {
+	hits map[string]int
+}
+
+func newSearchHits() *SearchHits {
+	return &SearchHits{
+		hits: make(map[string]int),
+	}
+}
+
+func (sh *SearchHits) appendTerm(term string, count int) {
+	sh.hits[term] = count
+}
+
+func (ti *TextIndex) match(qt *search.QueryTerm, hits *SearchHits) bool {
+	switch qt.Type() {
+	case search.WildCardQuery:
+		return ti.matchWildcard(qt, hits)
+	case search.PhraseQuery:
+		return ti.matchPhrase(qt, hits)
+	case search.FuzzyQuery:
+		return ti.matchFuzzy(qt, hits)
+	default:
+		// search.TermQuery is the default
+		return ti.matchTerm(qt, hits)
+	}
+}
+
+func (ti *TextIndex) matchPhrase(qt *search.QueryTerm, hits *SearchHits) bool {
+	var nextPositions []int
+
+	phrasePositions := map[int]string{}
+
+	words := strings.Fields(qt.Value)
+
+	if len(words) == 1 {
+		term, ok := ti.terms[qt.Value]
+		if !ok {
+			return false
+		}
+
+		hits.appendTerm(qt.Value, term.size())
+
+		return true
+	}
+
+	var previousTerm string
+
+	for idx, word := range words {
+		term, ok := ti.terms[word]
+		if !ok {
+			return false
+		}
+
+		if idx != 0 {
+			var wordMatch bool
+
+			for _, pos := range nextPositions {
+				// posMatch determines if this position can be followed
+				var posMatch bool
+
+				for _, nextPos := range search.ValidPhrasePosition(pos, qt.Slop) {
+					_, ok := term.position[nextPos]
+					if ok {
+						phrasePositions[nextPos] = word
+						posMatch = true
+						wordMatch = true
+					}
+				}
+
+				if posMatch {
+					phrasePositions[pos] = previousTerm
+				}
+			}
+
+			if !wordMatch {
+				return false
+			}
+		}
+
+		nextPositions = []int{}
+		for pos := range term.position {
+			nextPositions = append(nextPositions, pos)
+		}
+
+		previousTerm = word
+	}
+
+	matches := len(phrasePositions)
+
+	if matches != 0 {
+		phraseHits := sortAndCountPhrases(words, phrasePositions)
+
+		for k, v := range phraseHits {
+			hits.appendTerm(k, v)
+		}
+	}
+
+	return matches != 0
+}
+
+func sortAndCountPhrases(words []string, phrases map[int]string) map[string]int {
+	positions := []int{}
+	phraseSize := len(words)
+	phraseHits := map[string]int{}
+
+	for k := range phrases {
+		positions = append(positions, k)
+	}
+
+	sort.Slice(positions, func(i, j int) bool {
+		return positions[i] < positions[j]
+	})
+
+	phrase := []string{}
+
+	for idx, p := range positions {
+		phrase = append(phrase, phrases[p])
+		if len(phrase) != phraseSize {
+			if idx < len(positions) {
+				continue
+			}
+		}
+
+		currentPhrase := strings.Join(phrase, " ")
+		phraseHits[currentPhrase]++
+
+		phrase = []string{}
+	}
+
+	return phraseHits
+}
+
+func (ti *TextIndex) matchFuzzy(qt *search.QueryTerm, hits *SearchHits) bool {
+	var hasMatch bool
+
+	for k, v := range ti.terms {
+		ok, _ := search.IsFuzzyMatch(k, qt.Value, float64(qt.Fuzzy), search.Levenshtein)
+		if ok {
+			hasMatch = true
+
+			hits.appendTerm(k, v.size())
+		}
+	}
+
+	return hasMatch
+}
+
+func (ti *TextIndex) matchWildcard(qt *search.QueryTerm, hits *SearchHits) bool {
+	var matcher func(s, prefix string) bool
+
+	switch {
+	case qt.SuffixWildcard:
+		matcher = strings.HasSuffix
+	default:
+		matcher = strings.HasPrefix
+	}
+
+	var hasMatch bool
+
+	for k, v := range ti.terms {
+		if matcher(k, qt.Value) {
+			hasMatch = true
+
+			hits.appendTerm(k, v.size())
+		}
+	}
+
+	return hasMatch
+}
+
+func (ti *TextIndex) matchTerm(qt *search.QueryTerm, hits *SearchHits) bool {
+	term, ok := ti.terms[qt.Value]
+	if ok && qt.Prohibited {
+		return false
+	}
+
+	if !ok && !qt.Prohibited {
+		return false
+	}
+
+	var count int
+
+	if ok {
+		count = term.size()
+	}
+
+	hits.appendTerm(qt.Value, count)
+
+	return true
+}
+
+func (ti *TextIndex) Search(query *search.QueryTerm) (*SearchHits, error) {
+	hits := newSearchHits()
+	err := ti.search(query, hits)
+
+	return hits, err
+}
+
+func (ti *TextIndex) searchMustNot(query *search.QueryTerm, hits *SearchHits) error {
+	for _, qt := range query.MustNot() {
+		switch {
+		case qt.Type() == search.BoolQuery:
+			// TODO(kiivihal): find out why this is a dead branch
+			if err := ti.search(qt, hits); err != nil {
+				return err
+			}
+		default:
+			if !qt.Prohibited {
+				return fmt.Errorf("mustNot clauses must be marked as prohibited")
+			}
+
+			if ok := ti.match(qt, hits); !ok {
+				return ErrSearchNoMatch
+			}
+		}
+	}
+
+	return nil
+}
+
+func (ti *TextIndex) searchMust(query *search.QueryTerm, hits *SearchHits) error {
+	for _, qt := range query.Must() {
+		switch {
+		case qt.Type() == search.BoolQuery:
+			if err := ti.search(qt, hits); err != nil {
+				return err
+			}
+		default:
+			if ok := ti.match(qt, hits); !ok {
+				return ErrSearchNoMatch
+			}
+		}
+	}
+
+	return nil
+}
+
+func (ti *TextIndex) searchShould(query *search.QueryTerm, hits *SearchHits) error {
+	var matched bool
+
+	for _, qt := range query.Should() {
+		switch {
+		case qt.Type() == search.BoolQuery:
+			if err := ti.search(qt, hits); err != nil {
+				return err
+			}
+		default:
+			if ok := ti.match(qt, hits); ok {
+				matched = true
+			}
+		}
+	}
+
+	if !matched {
+		return ErrSearchNoMatch
+	}
+
+	return nil
+}
+
+// recursive search function
+func (ti *TextIndex) search(query *search.QueryTerm, hits *SearchHits) error {
+	if len(query.MustNot()) != 0 {
+		if err := ti.searchMustNot(query, hits); err != nil {
+			return err
+		}
+	}
+
+	if len(query.Must()) != 0 {
+		if err := ti.searchMust(query, hits); err != nil {
+			return err
+		}
+	}
+
+	if len(query.Should()) != 0 {
+		if err := ti.searchShould(query, hits); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// func (dc *DescriptionCounter) writeTo(w io.Writer) error {
+
+// jsonOutput, err := json.MarshalIndent(dc, "", " ")
+// if err != nil {
+// return fmt.Errorf("nable to marshall description to JSON; %w", err)
+// }
+
+// _, err = w.Write(jsonOutput)
+// if err != nil {
+// return fmt.Errorf("unable to write json; %w", err)
+// }
+
+// return nil
+// }
+
+// func (dc *DescriptionCounter) readFrom(r io.Reader) error {
+// d := json.NewDecoder(r)
+// return d.Decode(dc)
+// }
---
+++ ikuzo/storage/x/memory/index_full_text_test.go
@@ -0,0 +1,642 @@
+// nolint:gocritic
+package memory
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/matryer/is"
+	"github.com/delving/hub3/ikuzo/service/x/search"
+)
+
+var appendTests = []struct {
+	name    string
+	text    []byte
+	want    int
+	wantErr bool
+}{
+	{
+		"single word",
+		[]byte("word"),
+		1,
+		false,
+	},
+	{
+		"two words",
+		[]byte("two words"),
+		2,
+		false,
+	},
+	{
+		"one word with hyphen",
+		[]byte("two-words"),
+		3,
+		false,
+	},
+	{
+		"empty string cannot be indexed",
+		[]byte(".,;:"),
+		0,
+		true,
+	},
+}
+
+func TestTextIndex_appendBytes(t *testing.T) {
+	is := is.New(t)
+
+	for _, tt := range appendTests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			ti := NewTextIndex()
+
+			if err := ti.appendBytes(tt.text); (err != nil) != tt.wantErr {
+				t.Errorf("TextIndex.appendBytes() %s; error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			}
+
+			is.Equal(ti.size(), tt.want)
+		})
+	}
+}
+
+func TestTextIndex_appendString(t *testing.T) {
+	is := is.New(t)
+
+	for _, tt := range appendTests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			ti := NewTextIndex()
+
+			if err := ti.appendString(string(tt.text)); (err != nil) != tt.wantErr {
+				t.Errorf("TextIndex.appendBytes() %s; error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			}
+
+			is.Equal(ti.size(), tt.want)
+		})
+	}
+}
+
+func TestTextIndex_reset(t *testing.T) {
+	is := is.New(t)
+
+	ti := NewTextIndex()
+
+	is.Equal(ti.size(), 0)
+
+	err := ti.appendString("some words")
+	is.NoErr(err)
+
+	is.Equal(ti.size(), 2)
+
+	ti.reset()
+
+	is.Equal(ti.size(), 0)
+}
+
+type searchArgs struct {
+	query string
+	text  string
+}
+
+var searchTests = []struct {
+	name           string
+	args           searchArgs
+	want           map[string]int
+	wantErr        bool
+	wantNoMatchErr bool
+}{
+	{
+		"query not found",
+		searchArgs{
+			query: "ten",
+			text:  "zero to nine",
+		},
+		map[string]int{},
+		true,
+		true,
+	},
+	{
+		"must query not found",
+		searchArgs{
+			query: "+ten",
+			text:  "zero to nine",
+		},
+		map[string]int{},
+		true,
+		true,
+	},
+	{
+		"query excluded with match",
+		searchArgs{
+			query: "-zero",
+			text:  "zero to nine",
+		},
+		map[string]int{},
+		true,
+		true,
+	},
+	{
+		"query excluded with no match",
+		searchArgs{
+			query: "-ten",
+			text:  "zero to nine",
+		},
+		map[string]int{
+			"ten": 0,
+		},
+		false,
+		false,
+	},
+	{
+		"single must match",
+		searchArgs{
+			query: "+zero",
+			text:  "zero to nine",
+		},
+		map[string]int{
+			"zero": 1,
+		},
+		false,
+		false,
+	},
+	{
+		"single should match",
+		searchArgs{
+			query: "zero",
+			text:  "zero to nine",
+		},
+		map[string]int{
+			"zero": 1,
+		},
+		false,
+		false,
+	},
+	{
+		"multi-word should match",
+		searchArgs{
+			query: "zero to none",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"zero": 1,
+			"to":   2,
+		},
+		false,
+		false,
+	},
+	{
+		"multi-word should match",
+		searchArgs{
+			query: "zero to none",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"zero": 1,
+			"to":   2,
+		},
+		false,
+		false,
+	},
+	{
+		"prefix query",
+		searchArgs{
+			query: "zer* to none",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"zero":  1,
+			"zeros": 1,
+			"to":    2,
+		},
+		false,
+		false,
+	},
+	{
+		"suffix query",
+		searchArgs{
+			query: "*o none",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"zero": 1,
+			"to":   2,
+		},
+		false,
+		false,
+	},
+	{
+		"mixed query",
+		searchArgs{
+			query: "zero AND nine",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"zero": 1,
+			"nine": 1,
+		},
+		false,
+		false,
+	},
+	{
+		"mixed nested query",
+		searchArgs{
+			query: "(something OR zero) AND to",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"to":   2,
+			"zero": 1,
+		},
+		false,
+		false,
+	},
+	{
+		"mixed NOT query",
+		searchArgs{
+			query: "(something OR zero) AND -to",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{},
+		true,
+		true,
+	},
+	{
+		"mixed NOT query",
+		searchArgs{
+			query: "NOT (something OR zero) AND to",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"to":   2,
+			"zero": 1,
+		},
+		false,
+		false,
+	},
+	{
+		"fuzzy search",
+		searchArgs{
+			query: "zer~",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"zero":  1,
+			"zeros": 1,
+		},
+		false,
+		false,
+	},
+	{
+		"phrase search",
+		searchArgs{
+			query: "\"to nine\"",
+			text:  "zero to nine to zeros",
+		},
+		map[string]int{
+			"to nine": 1,
+		},
+		false,
+		false,
+	},
+}
+
+func TestTextIndex_search(t *testing.T) {
+	is := is.New(t)
+
+	// test prohibited
+	t.Run("mustNot should always have search.QueryTerm.Prohibited == true", func(t *testing.T) {
+		ti := NewTextIndex()
+		err := ti.appendString("something")
+		is.NoErr(err)
+
+		queryParser, err := search.NewQueryParser()
+		is.NoErr(err)
+
+		q, err := queryParser.Parse("-this")
+		is.NoErr(err)
+
+		for _, q := range q.MustNot() {
+			q.Prohibited = false
+		}
+
+		_, err = ti.Search(q)
+		if err == nil {
+			t.Errorf("TextIndex.Search() QueryTerm in mustNotClauses without Prohibited == true is not allowed")
+		}
+	})
+
+	for _, tt := range searchTests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			ti := NewTextIndex()
+
+			queryParser, err := search.NewQueryParser()
+			is.NoErr(err)
+
+			err = ti.appendString(tt.args.text)
+			is.NoErr(err)
+
+			query, err := queryParser.Parse(tt.args.query)
+			is.NoErr(err)
+
+			got, err := ti.Search(query)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TextIndex.search() %s error = %v, wantErr %v", tt.name, err, tt.wantErr)
+				return
+			}
+
+			if errors.Is(err, ErrSearchNoMatch) != tt.wantNoMatchErr {
+				t.Errorf("TextIndex.search() %s error = %v, wantNoMatchErr %v", tt.name, err, tt.wantErr)
+			}
+
+			if diff := cmp.Diff(tt.want, got.hits); diff != "" {
+				t.Errorf("TextIndex.search() %s = mismatch (-want +got):\n%s", tt.name, diff)
+			}
+		})
+	}
+}
+
+// nolint:funlen
+func TestTextIndex_matchCustom(t *testing.T) {
+	is := is.New(t)
+	ti := NewTextIndex()
+
+	type fields struct {
+		text string
+	}
+
+	type args struct {
+		qt   *search.QueryTerm
+		hits *SearchHits
+	}
+
+	tests := []struct {
+		name     string
+		fields   fields
+		args     args
+		matchFn  func(qt *search.QueryTerm, hits *SearchHits) bool
+		want     bool
+		wantHits *SearchHits
+	}{
+		{
+			"simple match",
+			fields{"zeeheldenkwartier held kwartier"},
+			args{
+				&search.QueryTerm{Value: "held"},
+				newSearchHits(),
+			},
+			ti.matchTerm,
+			true,
+			&SearchHits{hits: map[string]int{"held": 1}},
+		},
+		{
+			"no match",
+			fields{"zeeheldenkwartier held kwartier"},
+			args{
+				&search.QueryTerm{Value: "zeestraat"},
+				newSearchHits(),
+			},
+			ti.matchTerm,
+			false,
+			&SearchHits{hits: map[string]int{}},
+		},
+		{
+			"prohibited query",
+			fields{"zeeheldenkwartier held kwartier"},
+			args{
+				&search.QueryTerm{Value: "held", Prohibited: true},
+				newSearchHits(),
+			},
+			ti.matchTerm,
+			false,
+			&SearchHits{hits: map[string]int{}},
+		},
+		{
+			"prefix query",
+			fields{"zeeheldenkwartier held kwartier"},
+			args{
+				&search.QueryTerm{Value: "zee", PrefixWildcard: true},
+				newSearchHits(),
+			},
+			ti.matchWildcard,
+			true,
+			&SearchHits{hits: map[string]int{"zeeheldenkwartier": 1}},
+		},
+		{
+			"suffix query",
+			fields{"zeeheldenkwartier held kwartier"},
+			args{
+				&search.QueryTerm{Value: "kwartier", SuffixWildcard: true},
+				newSearchHits(),
+			},
+			ti.matchWildcard,
+			true,
+			&SearchHits{hits: map[string]int{"zeeheldenkwartier": 1, "kwartier": 1}},
+		},
+		{
+			"fuzzy query",
+			fields{"batauia"},
+			args{
+				&search.QueryTerm{Value: "batavia", Fuzzy: 1},
+				newSearchHits(),
+			},
+			ti.matchFuzzy,
+			true,
+			&SearchHits{hits: map[string]int{"batauia": 1}},
+		},
+		{
+			"fuzzy query (no match)",
+			fields{"Betuwe"},
+			args{
+				&search.QueryTerm{Value: "batavia", Fuzzy: 1},
+				newSearchHits(),
+			},
+			ti.matchFuzzy,
+			false,
+			&SearchHits{hits: map[string]int{}},
+		},
+		{
+			"phrase query (no match)",
+			fields{"ware helden van de zee, enteren de VOC. Ware helden zijn geen piraten."},
+			args{
+				&search.QueryTerm{Value: "ware held", Phrase: true},
+				newSearchHits(),
+			},
+			ti.matchPhrase,
+			false,
+			&SearchHits{hits: map[string]int{}},
+		},
+		{
+			"single word phrase query (match)",
+			fields{"ware helden van de zee, enteren de VOC. Ware helden zijn geen helden maar piraten."},
+			args{
+				&search.QueryTerm{Value: "de", Phrase: true},
+				newSearchHits(),
+			},
+			ti.matchPhrase,
+			true,
+			&SearchHits{hits: map[string]int{
+				"de": 2,
+			}},
+		},
+		{
+			"phrase query (match)",
+			fields{"ware helden van de zee, enteren de VOC. Ware helden zijn geen helden maar ware piraten."},
+			args{
+				&search.QueryTerm{Value: "ware helden", Phrase: true, Slop: 0},
+				newSearchHits(),
+			},
+			ti.matchPhrase,
+			true,
+			&SearchHits{hits: map[string]int{
+				"ware helden": 2,
+			}},
+		},
+		{
+			"phrase query (no match) without slop",
+			fields{"ware helden van de zee, enteren de VOC. Ware helden zijn geen helden, ware piraten."},
+			args{
+				&search.QueryTerm{Value: "helden van zee", Phrase: true, Slop: 0},
+				newSearchHits(),
+			},
+			ti.matchPhrase,
+			false,
+			&SearchHits{hits: map[string]int{}},
+		},
+		{
+			"phrase query (match) with slop",
+			fields{"ware helden van de zee, enteren de VOC. Ware helden zijn geen helden, ware piraten."},
+			args{
+				&search.QueryTerm{Value: "helden ware", Phrase: true, Slop: 1},
+				newSearchHits(),
+			},
+			ti.matchPhrase,
+			true,
+			&SearchHits{hits: map[string]int{
+				"ware helden": 2,
+				"helden ware": 1,
+			}},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			ti.reset()
+
+			err := ti.appendString(tt.fields.text)
+			is.NoErr(err)
+
+			if got := tt.matchFn(tt.args.qt, tt.args.hits); got != tt.want {
+				t.Errorf("TextIndex.match custom %s = %v, want %v", tt.name, got, tt.want)
+			}
+
+			if diff := cmp.Diff(tt.wantHits, tt.args.hits, cmp.AllowUnexported(SearchHits{})); diff != "" {
+				t.Errorf("TextIndex.match custom %s = mismatch (-want +got):\n%s", tt.name, diff)
+			}
+		})
+	}
+}
+
+func TestTextIndex_searchMustNot(t *testing.T) {
+	is := is.New(t)
+
+	type args struct {
+		query string
+		text  string
+		hits  *SearchHits
+	}
+
+	tests := []struct {
+		name           string
+		args           args
+		wantHits       *SearchHits
+		wantErr        bool
+		wantNoMatchErr bool
+	}{
+		{
+			"term query (match)",
+			args{
+				"-vrede",
+				"vredespaleis",
+				newSearchHits(),
+			},
+			&SearchHits{hits: map[string]int{"vrede": 0}},
+			false,
+			false,
+		},
+		{
+			"term query (no match)",
+			args{
+				"-vredespaleis",
+				"vredespaleis",
+				newSearchHits(),
+			},
+			&SearchHits{hits: map[string]int{}},
+			true,
+			true,
+		},
+		{
+			"boolean term query (no match)",
+			args{
+				"-word AND (-something AND word OR word3)",
+				"word something ガンダムバルバトス word3",
+				newSearchHits(),
+			},
+			&SearchHits{hits: map[string]int{}},
+			true,
+			true,
+		},
+		{
+			"boolean term query (no match)",
+			args{
+				"word1 OR (word2 OR (word4 OR word5))",
+				"word something ガンダムバルバトス word3",
+				newSearchHits(),
+			},
+			&SearchHits{hits: map[string]int{}},
+			true,
+			true,
+		},
+		{
+			"inverted boolean term query (no match)",
+			args{
+				"word AND NOT (word2 AND word3))",
+				"word1 something ガンダムバルバトス word3",
+				newSearchHits(),
+			},
+			&SearchHits{hits: map[string]int{}},
+			true,
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			ti := NewTextIndex()
+
+			queryParser, err := search.NewQueryParser()
+			is.NoErr(err)
+
+			err = ti.appendString(tt.args.text)
+			is.NoErr(err)
+
+			query, err := queryParser.Parse(tt.args.query)
+			is.NoErr(err)
+
+			if err := ti.search(query, tt.args.hits); (err != nil) != tt.wantErr {
+				t.Errorf("TextIndex.search() %s; error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			}
+
+			if diff := cmp.Diff(tt.wantHits, tt.args.hits, cmp.AllowUnexported(SearchHits{})); diff != "" {
+				t.Errorf("TextIndex.search() %s = mismatch (-want +got):\n%s", tt.name, diff)
+			}
+		})
+	}
+}
