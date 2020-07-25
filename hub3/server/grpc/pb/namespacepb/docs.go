905042de3c87b09a67b73dbfb447289be6779619
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2020-07-25 10:00:06 +0200

    Update `go generate` configurations during build.
    Some code is generated before the build. After removing the RAML
    api-console, this caused some build errors.

--- hub3/server/grpc/pb/namespacepb/docs.go
+++ hub3/server/grpc/pb/namespacepb/docs.go
@@ -16,7 +16,7 @@
 package namespacepb
 
 // generate grpc
-//go:generate 	protoc -I/usr/local/include -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. namespace.proto
+////go:generate 	protoc -I/usr/local/include -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. namespace.proto
 
 // generate reverse proxy gateway
 ////go:generate 	protoc -I/usr/local/include -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:.  namespace.proto
