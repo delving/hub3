d9c3d72e3cfbdbe86b91a14cba6e969254c2aa46
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2021-03-07 20:12:42 +0100

    Refactor sparql support into its own service.
    This is part of the process to phase out hub3/models package.

---
+++ ikuzo/service/x/sparql/options.go
@@ -0,0 +1,27 @@
+package sparql
+
+import (
+	"bytes"
+
+	"github.com/knakk/sparql"
+)
+
+type Option func(*Service) error
+
+// SetCustomQueries allows custom Sparql Queries to be added to the sparql.Bank
+//
+// Each query must be preceded by a comment otherwise the query is silently ignored.
+func SetCustomQueries(queries string) Option {
+	return func(s *Service) error {
+		f := bytes.NewBufferString(queries)
+		return s.mergeBank(sparql.LoadBank(f))
+	}
+}
+
+// SetTripleStore sets the TripleStore implementation for the sparql.Service
+func SetTripleStore(store TripleStore) Option {
+	return func(s *Service) error {
+		s.store = store
+		return nil
+	}
+}
