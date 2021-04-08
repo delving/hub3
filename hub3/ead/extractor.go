aa21bcc477025a69e69309313caa30e08bc281d2
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2021-04-08 10:42:48 +0200

    Better extraction of NLP entities from EAD.
    'geogname' and 'persname' are now added to the CLevel Resource Triples
    by default.

--- hub3/ead/extractor.go
+++ hub3/ead/extractor.go
@@ -15,8 +15,7 @@
 package ead
 
 import (
-	"bytes"
-	"encoding/xml"
+	"regexp"
 )
 
 type NLPType int
@@ -36,47 +35,27 @@ type NLPToken struct {
 }
 
 type Extractor struct {
-	XMLName   xml.Name     `xml:"input,omitempty" json:"input,omitempty"`
-	Cgeogname []*Cgeogname `xml:"geogname,omitempty" json:"geogname,omitempty"`
-	Cpersname []*Cpersname `xml:"persname,omitempty" json:"persname,omitempty"`
-	Cdate     []*Cdate     `xml:"date,omitempty" json:"date,omitempty"`
+	tokens []NLPToken
 }
 
 func NewExtractor(b []byte) (*Extractor, error) {
-	var buf bytes.Buffer
+	e := Extractor{tokens: []NLPToken{}}
 
-	buf.WriteString("<input>")
-	buf.Write(b)
-	buf.WriteString("</input>")
+	re := regexp.MustCompile(`<(geogname|persname)>(.*?)</(geogname|persname)>`)
+	matches := re.FindAllSubmatch(b, -1)
 
-	in := new(Extractor)
-
-	err := xml.Unmarshal(buf.Bytes(), in)
-	if err != nil {
-		return nil, err
-	}
-
-	return in, err
-}
-
-func (e *Extractor) Tokens() []NLPToken {
-	tokens := []NLPToken{}
-
-	for _, geo := range e.Cgeogname {
-		tokens = append(tokens, NLPToken{Text: geo.Geogname, Type: GeoLocation})
-	}
-
-	for _, pers := range e.Cpersname {
-		tokens = append(tokens, NLPToken{Text: pers.Persname, Type: Person})
-	}
-
-	for _, date := range e.Cdate {
-		tokens = append(tokens, NLPToken{Text: date.Date, Type: DateText})
-
-		if date.Attrnormal != "" {
-			tokens = append(tokens, NLPToken{Text: date.Attrnormal, Type: DateIso})
+	for _, m := range matches {
+		switch string(m[1]) {
+		case "geogname":
+			e.tokens = append(e.tokens, NLPToken{Text: string(m[2]), Type: GeoLocation})
+		case "persname":
+			e.tokens = append(e.tokens, NLPToken{Text: string(m[2]), Type: Person})
 		}
 	}
 
-	return tokens
+	return &e, nil
+}
+
+func (e *Extractor) Tokens() []NLPToken {
+	return e.tokens
 }
