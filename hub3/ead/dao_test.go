ea8a3b0ed430d2f1ce92b4044f50007f80c03c29
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2021-05-25 09:31:13 +0200

    Remove unused test.

--- hub3/ead/dao_test.go
+++ hub3/ead/dao_test.go
@@ -1,8 +1,9 @@
 package ead
 
 import (
-	"github.com/delving/hub3/hub3/ead/eadpb"
 	"testing"
+
+	"github.com/delving/hub3/hub3/ead/eadpb"
 )
 
 func TestFindDuplicatesInFilenames(t *testing.T) {
@@ -38,49 +39,6 @@ func TestFindDuplicatesInFilenames(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := assertUniqueFilenames(tt.items); (err != nil) != tt.wantErr {
 				t.Errorf("assertUniqueFilenames = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestReturnMapOfUniqueFilenamesWithTheirSortKey(t *testing.T) {
-	tests := []struct {
-		name    string
-		wantErr bool
-		items   []*eadpb.File
-	}{
-		{
-			"Should not throw error because of happy flow",
-			false,
-			[]*eadpb.File{
-				{Filename: "file_a", SortKey: 1},
-				{Filename: "file_a", SortKey: 2},
-			},
-		},
-		{
-			"Should throw error because the first sortKey is not as expected",
-			true,
-			[]*eadpb.File{
-				{Filename: "file_a", SortKey: 2},
-				{Filename: "file_a", SortKey: 3},
-			},
-		},
-		{
-			"Should throw error because the sortKeys are not in succeeding order",
-			true,
-			[]*eadpb.File{
-				{Filename: "file_a", SortKey: 1},
-				{Filename: "file_a", SortKey: 3},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		tt := tt
-
-		t.Run(tt.name, func(t *testing.T) {
-			if err := assertSortKeysAreOrdered(tt.items, 1); (err != nil) != tt.wantErr {
-				t.Errorf("assertSortKeysAreOrdered = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
