80c4d3be5eb1ffa164c7dd11665d5f13bc95c1b1
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2022-01-25 11:06:01 +0100

    hub3: added sitemap package
    This is an initial version.

---
+++ ikuzo/service/x/sitemap/handle_sitemap.go
@@ -0,0 +1,48 @@
+package sitemap
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/delving/hub3/ikuzo/domain"
+	"github.com/go-chi/chi"
+)
+
+var ErrConfigNotFound = errors.New("sitemap configuration not found")
+
+// handleBaseSitemap is the entry point for all sub-sitemaps.
+func (s *Service) handleBaseSitemap(w http.ResponseWriter, r *http.Request) {
+	cfg, err := s.sitemapConfig(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	sm, err := s.sitemapRoot(r.Context(), cfg)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+	sm.WriteTo(w)
+}
+
+func (s *Service) sitemapConfig(r *http.Request) (Config, error) {
+	var cfg Config
+
+	org, ok := domain.GetOrganization(r)
+	if !ok {
+		return cfg, ErrConfigNotFound
+	}
+
+	configID := chi.URLParam(r, configIDKey)
+	for _, sitemap := range org.Config.Sitemaps {
+		if strings.EqualFold(sitemap.ID, configID) {
+			sitemap.OrgID = org.RawID()
+			return sitemap, nil
+		}
+	}
+
+	return cfg, ErrConfigNotFound
+}
