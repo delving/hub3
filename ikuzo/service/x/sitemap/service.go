f7f5f3f921783fa74594cd79dcbdeb9ea363ab44
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2022-01-25 11:06:01 +0100

    hub3: added sitemap package
    This is an initial version.

---
+++ ikuzo/service/x/sitemap/service.go
@@ -0,0 +1,70 @@
+package sitemap
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/delving/hub3/ikuzo/domain"
+	"github.com/go-chi/chi"
+	"github.com/rs/zerolog"
+	"github.com/snabb/sitemap"
+)
+
+var _ domain.Service = (*Service)(nil)
+
+type Service struct {
+	store Store
+	orgs  domain.OrgConfigRetriever
+	log   zerolog.Logger
+}
+
+func NewService(options ...Option) (*Service, error) {
+	s := &Service{}
+
+	// apply options
+	for _, option := range options {
+		if err := option(s); err != nil {
+			return nil, err
+		}
+	}
+
+	return s, nil
+}
+
+func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	router := chi.NewRouter()
+	s.Routes("", router)
+	router.ServeHTTP(w, r)
+}
+
+func (s *Service) Shutdown(ctx context.Context) error {
+	return nil
+}
+
+func (s *Service) sitemapRoot(ctx context.Context, cfg Config) (*sitemap.SitemapIndex, error) {
+	datasets, err := s.store.Datasets(ctx, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	smi := sitemap.NewSitemapIndex()
+
+	for _, d := range datasets {
+		smi.Add(&sitemap.URL{
+			Loc: fmt.Sprintf(
+				"%s/api/sitemap/%s/%s",
+				cfg.BaseURL,
+				cfg.ID,
+				d.ID,
+			),
+		})
+	}
+
+	return smi, nil
+}
+
+func (s *Service) SetServiceBuilder(b *domain.ServiceBuilder) {
+	s.log = b.Logger.With().Str("svc", "sitemap").Logger()
+	s.orgs = b.Orgs
+}
---
+++ ikuzo/service/x/sitemap/store.go
@@ -0,0 +1,17 @@
+package sitemap
+
+import (
+	"context"
+	"time"
+)
+
+type Store interface {
+	Datasets(ctx context.Context, cfg Config) (locations []Location, err error)
+	LocationCount(ctx context.Context, cfg Config) (int, error)
+	Locations(ctx context.Context, cfg Config, start, end int) []Location
+}
+
+type Location struct {
+	ID      string // relative path to unique identifier
+	LastMod *time.Time
+}
