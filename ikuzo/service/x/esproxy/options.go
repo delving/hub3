10f521cfd69ae861dc37e0de1431b96ee3daa900
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2022-01-25 11:07:09 +0100

    hub3: added dedicade esproxy service.

---
+++ ikuzo/service/x/esproxy/options.go
@@ -0,0 +1,19 @@
+package esproxy
+
+import "github.com/delving/hub3/ikuzo/driver/elasticsearch"
+
+type Option func(*Service) error
+
+func SetElasticClient(es *elasticsearch.Client) Option {
+	return func(s *Service) error {
+		s.es = es
+		return nil
+	}
+}
+
+func SetEnableIntrospect(enabled bool) Option {
+	return func(s *Service) error {
+		s.introspect = enabled
+		return nil
+	}
+}
