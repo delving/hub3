41309cb09084b8fa3b5439cc0e64f165d7848911
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2022-01-25 10:52:56 +0100

    hub3: initial version of postgresql service.

---
+++ ikuzo/storage/x/postgresql/db.go
@@ -0,0 +1,48 @@
+package postgresql
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	// import the sql.DB driver for postgresql
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	DSN          string
+	MaxOpenConns int
+	MaxIdleConns int
+	MaxIdleTime  string
+}
+
+func OpenDB(cfg Config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.MaxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := EnsureSchema(cfg.DSN); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
