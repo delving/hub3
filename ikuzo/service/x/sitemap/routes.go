5af52e3ebf858afce9ab30cbf745ff12d0e91b0a
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2022-02-14 10:44:45 +0100

    hub3: changed sitemap package to support stores.
    This commit also contains the initial implementation of an elasticsearch
    based sitemap store. The file package will also contain a sitemap based
    on the RecordIndex which will be the default in the future.
    
    The elasticsearch package will be used for dynamic sitemaps.

--- ikuzo/service/x/sitemap/routes.go
+++ ikuzo/service/x/sitemap/routes.go
@@ -5,8 +5,7 @@ import "github.com/go-chi/chi"
 const configIDKey = "configID"
 
 func (s *Service) Routes(pattern string, router chi.Router) {
-	router.Get("/api/sitemap", s.handleBaseSitemap)
-	// TODO(kiivihal): implement routes
+	router.Get("/api/sitemap", s.handleListSitemapKeys)
 	router.Get("/api/sitemap/{configID}", s.handleBaseSitemap)
 	router.Get("/api/sitemap/{configID}/{datasetID}", s.handleBaseSitemap)
 	router.Get("/api/sitemap/{configID}/{datasetID}/{page}", s.handleBaseSitemap)
