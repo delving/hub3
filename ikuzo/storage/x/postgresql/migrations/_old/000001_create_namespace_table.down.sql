f485643c20a97ba936739d500ac3e3765334371f
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2022-08-30 11:04:07 +0200

    wip: added configuration from stash.

--- ikuzo/storage/x/postgresql/migrations/_old/000001_create_namespace_table.down.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-DROP TABLE IF EXISTS namespace;
-DROP TABLE IF EXISTS namespace_history;
-DROP TRIGGER IF EXISTS namespace_hist_trigger on namespace;
-DROP EXTENSION IF EXISTS temporal_tables;
-DROP EXTENSION IF EXISTS "uuid-ossp";
-END;
