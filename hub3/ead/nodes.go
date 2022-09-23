f23dccd4ae6dc65a0b1627b763054f958f8d49c7
Bram Bimmerman <bram.bimmerman@ictu.nl>
2022-09-23 10:38:44 +0200

    if dit?

--- hub3/ead/nodes.go
+++ hub3/ead/nodes.go
@@ -247,6 +247,11 @@ func CreateTree(cfg *NodeConfig, n *Node, hubID string, id string) *fragments.Tr
 		}
 	}
 
+	if len(config.Config.EAD.Genreforms) == 0 {
+		tree.Genreform = n.Header.Genreform
+		return tree
+	}
+
 	if len(config.Config.EAD.Genreforms) > 1 {
 		var b bool
 		for _, a := range config.Config.EAD.Genreforms {
