7b8bef2c84a84e0b087b0f4aa7640ad06249ac16
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2022-10-14 11:44:25 +0200

    add stream test

---
+++ ikuzo/driver/elasticsearch/stream_test.go
@@ -0,0 +1,49 @@
+package elasticsearch
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/matryer/is"
+	"github.com/olivere/elastic/v7"
+)
+
+func TestStreamFragments(t *testing.T) {
+	is := is.New(t)
+
+	testSets := []string{
+		"mauritshuis",
+		// "museum-de-fundatie",
+		// "rijksmuseum",
+		// "catharijneconvent",
+		// "stedelijk-museum-schiedam",
+		// "van-abbe-museum",
+		// "museum-belvedere",
+		// "rijksakademie",
+		// "moderne-kunst-museum-deventer",
+	}
+
+	query := elastic.NewBoolQuery()
+	for _, setSpec := range testSets {
+		query.Should(elastic.NewTermQuery("meta.spec", setSpec))
+	}
+
+	cfg := Config{Urls: []string{"http://localhost:9200"}}
+	client, err := NewClient(&cfg)
+	is.NoErr(err)
+
+	streamConfig := &StreamConfig{
+		Query:      query,
+		IndexNames: []string{"dcnv2_20200526062245.616"},
+	}
+
+	printFunc := func(hit *elastic.SearchHit) error {
+		log.Printf("hit: %s", hit.Id)
+		return nil
+	}
+
+	seen, streamErr := client.Stream(context.Background(), streamConfig, printFunc)
+	is.NoErr(streamErr)
+	is.Equal(seen, 856)
+}
