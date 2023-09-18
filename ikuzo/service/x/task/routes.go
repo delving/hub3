d01863a803b0822226816fc1e5d042a30de344e9
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2023-09-18 20:08:53 +0200

    feat(task): add asynq monitor to task routes

--- ikuzo/service/x/task/routes.go
+++ ikuzo/service/x/task/routes.go
@@ -1,6 +1,16 @@
 package task
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/hibiken/asynqmon"
+)
 
 func (s *Service) Routes(pattern string, r chi.Router) {
+	h := asynqmon.New(asynqmon.Options{
+		RootPath:     "/monitoring", // RootPath specifies the root for asynqmon app
+		RedisConnOpt: s.redisClientOpt(),
+	})
+
+	r.Handle("/monitoring", h)
+	r.Handle("/monitoring/*", h)
 }
