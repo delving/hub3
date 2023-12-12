bfb943f9a04f8988f2ce72e9c5f822c66aaee112
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2023-12-12 13:45:08 +0100

    fix(ikuzo): only start background workers when task.Service is not nil

--- ikuzo/workers.go
+++ ikuzo/workers.go
@@ -2,6 +2,10 @@ package ikuzo
 
 func (s *server) BackgroundWorkers() error {
 	s.logger.Info().Msg("starting hibiken/asynq background workers")
+	if s.ts == nil {
+		s.logger.Warn().Msg("cannot start background workers when task.Service is not initialised")
+		return nil
+	}
 	defer s.shutdown(nil)
 	if err := s.ts.StartWorkers(s.ctx); err != nil {
 		return err
