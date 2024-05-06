1a99aaaf5671001aed4f9fa55ee8af1532d4bcb6
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2024-05-06 09:15:40 +0200

    feat: scaffolding for the rdf-based search APIs.

---
+++ ikuzo/rdf/api/docs.go
@@ -0,0 +1,6 @@
+// package api contains shared functionality between different api version
+//
+// The api package is placed here because it is connected to the index package.
+// That package provides the structure how RDF data is indexed and therefore how
+// you can search in it.
+package api
