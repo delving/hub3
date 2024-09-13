cf5e5870ae7cb18e9184814f62a21b80077b8500
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2024-09-13 09:48:42 +0200

    fix: make org routes only available in debug mode

--- ikuzo/service/organization/routes.go
+++ ikuzo/service/organization/routes.go
@@ -1,15 +1,21 @@
 package organization
 
-import "github.com/go-chi/chi/v5"
+import (
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func (s *Service) Routes(pattern string, r chi.Router) {
 	if pattern == "" {
 		pattern = "/organizations"
 	}
 
-	r.Route(pattern, func(router chi.Router) {
-		router.Get("/", s.handleFilter)
-		router.Get("/{id}", s.handleGet)
-		router.Put("/", s.handlePut)
-	})
+	if strings.EqualFold("HUB3_ORG_DEBUG", "true") {
+		r.Route(pattern, func(router chi.Router) {
+			router.Get("/", s.handleFilter)
+			router.Get("/{id}", s.handleGet)
+			router.Put("/", s.handlePut)
+		})
+	}
 }
