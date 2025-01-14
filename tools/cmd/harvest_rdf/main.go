18a34f3b92277a437567677d97ea1e7438cc91cf
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2025-01-14 11:05:35 +0100

    feat: add support for harvesting v1 API for rdf-dump
    Change-Id: I981ead4f20ce25e984b04efde0243d93676f62ac

--- tools/cmd/harvest_rdf/main.go
+++ tools/cmd/harvest_rdf/main.go
@@ -10,18 +10,23 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/delving/hub3/hub3/fragments"
+	"github.com/delving/hub3/ikuzo/rdf/formats/jsonld"
 	"github.com/delving/hub3/ikuzo/rdf/formats/ntriples"
+	"github.com/tidwall/gjson"
 )
 
 // main is the entry point of the application.
 func main() {
 	var apiURL, outputDir, fileLabel string
+	var v1Mode bool
 	flag.StringVar(&apiURL, "url", "", "API URL to fetch data from")
 	flag.StringVar(&outputDir, "output", "output", "Output directory to save files")
 	flag.StringVar(&fileLabel, "label", "", "Descriptive label to be used in the filename")
+	flag.BoolVar(&v1Mode, "v1", false, "Harvest in v1.mode")
 
 	flag.Parse()
 
@@ -30,14 +35,147 @@ func main() {
 		return
 	}
 
-	err := harvestData(apiURL, outputDir, fileLabel)
+	if v1Mode {
+		err := harvestV1Data(apiURL, outputDir, fileLabel)
+		if err != nil {
+			slog.Error("could not harvest data", "error", err)
+		}
+		return
+	}
+
+	err := harvestV2Data(apiURL, outputDir, fileLabel)
 	if err != nil {
 		slog.Error("could not harvest data", "error", err)
 	}
 }
 
-// harvestData fetches data from the API and saves it to the specified output directory.
-func harvestData(apiURL, outputDir, fileLabel string) error {
+// harvestV1Data fetches data from the v1 API and saves it to the specified output directory.
+func harvestV1Data(apiURL, outputDir, fileLabel string) error {
+	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+		slog.Error("failed to create output directory", "error", err)
+		return err
+	}
+
+	output, err := os.Create(ntriplesFname(outputDir, fileLabel))
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+	defer output.Close()
+
+	var nextPage, seen, pages int
+	nextPage = 1
+
+	for {
+		resp, err := getV1APIResponse(apiURL, nextPage)
+		if err != nil {
+			return fmt.Errorf("error retrieving v1 response: %w", err)
+		}
+		pages++
+
+		nextPage = resp.NextPage
+
+		for _, graph := range resp.Graphs {
+			seen++
+			g, err := jsonld.Parse(strings.NewReader(graph), nil)
+			if err != nil {
+				return fmt.Errorf("unable to parse json-ld sting: %w \n %s", err, graph)
+			}
+
+			if err := ntriples.SerializeFiltered(g, output, "<urn:private"); err != nil {
+				return fmt.Errorf("failed to serialize graph: %w", err)
+			}
+		}
+		if seen%500 == 0 {
+			slog.Info("progress update", "seen", seen, "pages", pages, "total", resp.NumFound)
+		}
+
+		if resp.EOF {
+			break
+		}
+	}
+
+	return nil
+}
+
+type v1Response struct {
+	NumFound int
+	NextPage int
+	Graphs   []string
+	EOF      bool
+}
+
+// getV1APIResponse makes an HTTP GET request to the API and returns the response.
+func getV1APIResponse(uri string, page int) (data *v1Response, err error) {
+	data = &v1Response{}
+	if page != 0 {
+		apiURI, err := url.Parse(uri)
+		if err != nil {
+			return data, fmt.Errorf("failed to parse URL: %w", err)
+		}
+		params := apiURI.Query()
+		params.Set("page", fmt.Sprintf("%d", page))
+		apiURI.RawQuery = params.Encode()
+		uri = apiURI.String()
+	}
+
+	slog.Info("retrieving page", "uri", uri)
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		return data, fmt.Errorf("failed to make the API request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return data, fmt.Errorf("failed to read error response body: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		slog.Error("error response from API", "uri", uri, "response", string(body))
+		return data, fmt.Errorf("unable to retrieve API: %s", uri)
+	}
+
+	numFoundRaw := gjson.GetBytes(body, "@dig:pagination|@flatten.0.numFound")
+	if !numFoundRaw.Exists() {
+		return data, fmt.Errorf("unable to find numfound in API response: %s", uri)
+	}
+
+	slog.Info("numFound", "numFound", numFoundRaw.Int(), "raw", numFoundRaw.String())
+
+	data.NumFound = int(numFoundRaw.Int())
+
+	graphsRaw := gjson.GetBytes(body, "@dig:system.source_graph|@flatten")
+	if !graphsRaw.Exists() {
+		return data, fmt.Errorf("unable to find source_graph in API response: %s", uri)
+	}
+
+	for _, graph := range graphsRaw.Array() {
+		data.Graphs = append(data.Graphs, graph.String())
+	}
+
+	hasNext := gjson.GetBytes(body, "@dig:pagination|@flatten.0.hasNext")
+	if !hasNext.Exists() {
+		return data, fmt.Errorf("unable to find hasNext in API response: %s", uri)
+	}
+
+	if !hasNext.Bool() {
+		data.EOF = true
+		return data, nil
+	}
+
+	nextPage := gjson.GetBytes(body, "@dig:pagination|@flatten.0.nextPageNumber")
+	if !nextPage.Exists() {
+		return data, fmt.Errorf("unable to find nextPage in API response: %s", uri)
+	}
+
+	data.NextPage = int(nextPage.Int())
+
+	return data, nil
+}
+
+// harvestV2Data fetches data from the API and saves it to the specified output directory.
+func harvestV2Data(apiURL, outputDir, fileLabel string) error {
 	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
 		slog.Error("failed to create output directory", "error", err)
 		return err
@@ -53,7 +191,7 @@ func harvestData(apiURL, outputDir, fileLabel string) error {
 	var pages int
 	var scrollID string
 	for {
-		resp, err := getAPIResponse(apiURL, scrollID)
+		resp, err := getV2APIResponse(apiURL, scrollID)
 		if err != nil {
 			return fmt.Errorf("failed to get API response: %w", err)
 		}
@@ -84,8 +222,8 @@ func harvestData(apiURL, outputDir, fileLabel string) error {
 	return nil
 }
 
-// getAPIResponse makes an HTTP GET request to the API and returns the response.
-func getAPIResponse(uri, scrollID string) (*fragments.ScrollResultV4, error) {
+// getV2APIResponse makes an HTTP GET request to the API and returns the response.
+func getV2APIResponse(uri, scrollID string) (*fragments.ScrollResultV4, error) {
 	if scrollID != "" {
 		apiURI, err := url.Parse(uri)
 		if err != nil {
