88c1b602e44a8ee454cbcda129760b172f1e0812
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2026-04-23 11:57:43 +0200

    feat(bulk): add HubIDs field to Request for drop_records

--- ikuzo/service/x/bulk/request_test.go
+++ ikuzo/service/x/bulk/request_test.go
@@ -1,6 +1,7 @@
 package bulk
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
@@ -49,4 +50,16 @@ func TestParseGraph(t *testing.T) {
 		}
 	}
 	is.Equal(len(cho.Entries), 15)
+}
+
+func TestRequestDecodesHubIDsField(t *testing.T) {
+	is := is.New(t)
+	body := `{"action":"drop_records","orgID":"o","dataset":"d","hubIds":["o_d_a","o_d_b"]}`
+	var req Request
+	err := json.Unmarshal([]byte(body), &req)
+	is.NoErr(err)
+	is.Equal(req.Action, "drop_records")
+	is.Equal(len(req.HubIDs), 2)
+	is.Equal(req.HubIDs[0], "o_d_a")
+	is.Equal(req.HubIDs[1], "o_d_b")
 }
