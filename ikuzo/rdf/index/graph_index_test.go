delving/hub3

commit 59f12715078ac9cec0bd57f5475e7d51cb41894e

feat: added support for tagmap and flat fields during indexing
Change-Id: Ib248fa37032b69412531fb17a907c122c1cf502a

ikuzo/rdf/index/graph_index_test.go
@@ -34,7 +34,7 @@ func TestGraph_IndexMessage(t *testing.T) {
 
 	// Root subject
 	rootSubject, _ := rdf.NewIRI("urn:test:subject")
-	
+
 	// Define a type for root
 	rdfType, _ := rdf.NewIRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 	rootTypeObj, _ := rdf.NewIRI("http://example.org/ontology/RootType")
@@ -68,14 +68,14 @@ func TestGraph_IndexMessage(t *testing.T) {
 	// Get the IndexMessage with nil TagMap
 	indexMsg, err := g.IndexMessage(nil)
 	is.NoErr(err)
-	
+
 	// Verify that contextIsSet is now true after IndexMessage
 	is.Equal(g.contextIsSet, true)
 
 	// This test verifies that IndexMessage calls addContextLevels
 	// The actual setting of context levels depends on the library implementation
 	// which seems to have issues in the test environment
-	
+
 	// Verify we got a valid index message
 	is.True(indexMsg != nil)
 	is.Equal(indexMsg.OrganisationID, "test-org")
@@ -131,11 +131,11 @@ func TestAddContextLevels(t *testing.T) {
 		EntryType:   ResourceType,
 		ID:          "urn:test:level1B",
 	})
-	
+
 	// Verify objectIDs returns the expected connections
 	objectIDs := rootResource.objectIDs()
 	is.Equal(len(objectIDs), 2)
-	
+
 	// Verify the first object ID
 	found1A := false
 	found1B := false
@@ -154,20 +154,190 @@ func TestAddContextLevels(t *testing.T) {
 
 	// Verify contextIsSet is initially false
 	is.Equal(g.contextIsSet, false)
-	
+
 	// Call addContextLevels
 	err = g.addContextLevels()
 	is.NoErr(err)
-	
+
 	// Verify contextIsSet is now true
 	is.True(g.contextIsSet)
-	
+
 	// Create a JSON representation and verify it can be marshaled
 	jsonBytes, err := json.MarshalIndent(g, "", "  ")
 	is.NoErr(err)
 	is.True(len(jsonBytes) > 0)
 
+	is.True(len(g.Resources[0].Context) > 0)
+
 	// This verifies the basic functionality of addContextLevels
 	// The actual context references might not be set in the test environment
 	// but the flag is correctly updated
+}
+
+// TestGraph_ContextLevelsInResources tests that context levels are properly set in Resources
+func TestGraph_ContextLevelsInResources(t *testing.T) {
+	is := is.New(t)
+
+	// Create a test graph with a nested structure to test context levels
+	header := Header{
+		OrgID:    "test-org",
+		Spec:     "test-spec",
+		HubID:    "test-hubid",
+		EntryURI: "urn:test:root", // This is our root resource
+	}
+
+	g, err := NewGraph(header)
+	is.NoErr(err)
+
+	// Create a 3-level deep resource structure:
+	// Root -> Level1 -> Level2
+
+	// Create root resource
+	rootResource := &Resource{
+		ID:    "urn:test:root",
+		Types: []string{"http://example.org/ontology/Root"},
+	}
+	g.Resources = append(g.Resources, rootResource)
+
+	// Create level 1 resource
+	level1Resource := &Resource{
+		ID:    "urn:test:level1",
+		Types: []string{"http://example.org/ontology/Level1"},
+	}
+	g.Resources = append(g.Resources, level1Resource)
+
+	// Create level 2 resource
+	level2Resource := &Resource{
+		ID:    "urn:test:level2",
+		Types: []string{"http://example.org/ontology/Level2"},
+	}
+	g.Resources = append(g.Resources, level2Resource)
+
+	// Add connections between resources
+	rootResource.Entries = append(rootResource.Entries, &Entry{
+		Predicate:   "http://example.org/ontology/hasLevel1",
+		SearchLabel: "test_hasLevel1",
+		EntryType:   ResourceType,
+		ID:          "urn:test:level1",
+	})
+
+	level1Resource.Entries = append(level1Resource.Entries, &Entry{
+		Predicate:   "http://example.org/ontology/hasLevel2",
+		SearchLabel: "test_hasLevel2",
+		EntryType:   ResourceType,
+		ID:          "urn:test:level2",
+	})
+
+	// Verify contextIsSet is initially false
+	is.Equal(g.contextIsSet, false)
+
+	// Add context levels
+	err = g.addContextLevels()
+	is.NoErr(err)
+
+	// Verify contextIsSet is now true
+	is.Equal(g.contextIsSet, true)
+
+	// Based on our debugging, we now understand that this implementation stores
+	// context references in the GraphExternalContext field of the resource that
+	// "owns" the relation, rather than in the Context field of the target resource
+
+	// Verify that the root resource has a GraphExternalContext entry for level1
+	is.True(len(rootResource.GraphExternalContext) > 0)
+
+	// Find and validate the context reference for level1
+	foundLevel1Context := false
+	for _, ctx := range rootResource.GraphExternalContext {
+		if ctx.ObjectID == "urn:test:level1" && ctx.Predicate == "http://example.org/ontology/hasLevel1" {
+			foundLevel1Context = true
+			is.Equal(ctx.Subject, "urn:test:root")
+			is.Equal(ctx.Level, int32(1)) // Level should be 1
+			is.Equal(len(ctx.SubjectClass), 1)
+			is.Equal(ctx.SubjectClass[0], "http://example.org/ontology/Root")
+		}
+	}
+	is.True(foundLevel1Context)
+
+	// Test that the context flag is set correctly
+	// This confirms that the addContextLevels function was called and completed successfully
+	is.True(g.contextIsSet)
+}
+
+// TestGraph_IndexMessageWithFields tests that fields are properly generated and included in the index message
+func TestGraph_IndexMessageWithFields(t *testing.T) {
+	is := is.New(t)
+
+	// Create a test graph with literal values
+	header := Header{
+		OrgID:    "test-org",
+		Spec:     "test-spec",
+		HubID:    "test-hubid",
+		EntryURI: "urn:test:root",
+	}
+
+	g, err := NewGraph(header)
+	is.NoErr(err)
+
+	// Create root resource
+	rootResource := &Resource{
+		ID:    "urn:test:root",
+		Types: []string{"http://example.org/ontology/TestType"},
+	}
+	g.Resources = append(g.Resources, rootResource)
+
+	// Add some literal entries with different predicates
+	rootResource.Entries = append(rootResource.Entries, &Entry{
+		Predicate:   "http://purl.org/dc/elements/1.1/title",
+		SearchLabel: "dc_title",
+		Value:       "Test Title",
+		EntryType:   Literal,
+	})
+
+	rootResource.Entries = append(rootResource.Entries, &Entry{
+		Predicate:   "http://purl.org/dc/elements/1.1/title",
+		SearchLabel: "dc_title",
+		Value:       "Another Title", // Different value, same predicate
+		EntryType:   Literal,
+	})
+
+	rootResource.Entries = append(rootResource.Entries, &Entry{
+		Predicate:   "http://purl.org/dc/elements/1.1/date",
+		SearchLabel: "dc_date",
+		Value:       "2020-01-01",
+		EntryType:   Literal,
+	})
+
+	// Verify Fields is initially nil
+	is.True(g.Fields == nil)
+
+	// Create index message (this should generate fields)
+	indexMsg, err := g.IndexMessage(nil)
+	is.NoErr(err)
+	is.True(indexMsg != nil)
+
+	// Unmarshal the source to verify Fields is included
+	var graphData map[string]interface{}
+	err = json.Unmarshal(indexMsg.Source, &graphData)
+	is.NoErr(err)
+
+	// Check fields exists and has correct content
+	fields, ok := graphData["fields"].(map[string]interface{})
+	is.True(ok) // Fields should be in the JSON
+
+	// Check that the fields contain both predicates with correct values
+	titleValues, ok := fields["http://purl.org/dc/elements/1.1/title"].([]interface{})
+	is.True(ok)
+	is.Equal(len(titleValues), 2) // Should have both title values
+
+	// Check that dc:date has one value
+	dateValues, ok := fields["http://purl.org/dc/elements/1.1/date"].([]interface{})
+	is.True(ok)
+	is.Equal(len(dateValues), 1)
+	is.Equal(dateValues[0], "2020-01-01")
+
+	// Verify that g.Fields was populated
+	is.True(g.Fields != nil)
+	is.Equal(len(g.Fields), 2) // Should have dc:title and dc:date
+	is.Equal(len(g.Fields["http://purl.org/dc/elements/1.1/title"]), 2)
+	is.Equal(len(g.Fields["http://purl.org/dc/elements/1.1/date"]), 1)
 }

go.mod
@@ -81,7 +81,6 @@ require (
 )
 
 require (
-	github.com/BurntSushi/toml v1.5.0 // indirect
 	github.com/air-verse/air v1.61.7 // indirect
 	github.com/bep/godartsass v1.2.0 // indirect
 	github.com/bep/godartsass/v2 v2.1.0 // indirect

go.sum
@@ -1052,6 +1052,8 @@ github.com/prometheus/procfs v0.6.0/go.mod h1:cz+aTbrPOrUb4q7XlbU9ygM+/jj0fzG6c1
 github.com/prometheus/procfs v0.7.3/go.mod h1:cz+aTbrPOrUb4q7XlbU9ygM+/jj0fzG6c1xBZuNvfVA=
 github.com/prometheus/procfs v0.15.1 h1:YagwOFzUgYfKKHX6Dr+sHT7km/hxC76UB0learggepc=
 github.com/prometheus/procfs v0.15.1/go.mod h1:fB45yRUv8NstnjriLhBQLuOUt+WW4BsoGhij/e3PBqk=
+github.com/rakyll/statik v0.1.7 h1:OF3QCZUuyPxuGEP7B4ypUa7sB/iHtqOTDYZXGM8KOdQ=
+github.com/rakyll/statik v0.1.7/go.mod h1:AlZONWzMtEnMs7W4e/1LURLiI49pIMmp6V9Unghqrcc=
 github.com/redis/go-redis/v9 v9.0.3/go.mod h1:WqMKv5vnQbRuZstUwxQI195wHy+t4PuXDOjzMvcuQHk=
 github.com/redis/go-redis/v9 v9.0.4/go.mod h1:WqMKv5vnQbRuZstUwxQI195wHy+t4PuXDOjzMvcuQHk=
 github.com/redis/go-redis/v9 v9.5.3 h1:fOAp1/uJG+ZtcITgZOfYFmTKPE7n4Vclj1wZFgRciUU=
@@ -1305,8 +1307,6 @@ golang.org/x/crypto v0.3.1-0.20221117191849-2c476679df9a/go.mod h1:hebNnKkNXi2Uz
 golang.org/x/crypto v0.5.0/go.mod h1:NK/OQwhpMQP3MwtdjgLlYHnH9ebylxKWv3e0fK+mkQU=
 golang.org/x/crypto v0.7.0/go.mod h1:pYwdfH91IfpZVANVyUOhSIPZaFoJGxTFbZhFTx+dXZU=
 golang.org/x/crypto v0.9.0/go.mod h1:yrmDGqONDYtNj3tH8X9dzUun2m2lzPa9ngI6/RUPGR0=
-golang.org/x/crypto v0.31.0 h1:ihbySMvVjLAeSH1IbfcRTkD/iNscyz8rGzjF/E5hV6U=
-golang.org/x/crypto v0.31.0/go.mod h1:kDsLvtWBEx7MV9tJOj9bnXsPbxwJQ6csT/x4KIN4Ssk=
 golang.org/x/crypto v0.38.0 h1:jt+WWG8IZlBnVbomuhg2Mdq0+BBQaHbtqHEFEigjUV8=
 golang.org/x/crypto v0.38.0/go.mod h1:MvrbAqul58NNYPKnOra203SB9vpuZW0e+RRZV+Ggqjw=
 golang.org/x/exp v0.0.0-20180321215751-8460e604b9de/go.mod h1:CJ0aWSM057203Lf6IL+f9T1iT9GByDxfZKAQTCR3kQA=
@@ -1364,8 +1364,6 @@ golang.org/x/mod v0.4.2/go.mod h1:s0Qsj1ACt9ePp/hMypM3fl4fZqREWJwdYDEqhRiZZUA=
 golang.org/x/mod v0.5.0/go.mod h1:5OXOZSfqPIIbmVBIIKWRFfZjPR0E5r58TLhUjH0a2Ro=
 golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4/go.mod h1:jJ57K6gSWd91VN4djpZkiMVwK6gcyfeH4XE8wZrZaV4=
 golang.org/x/mod v0.8.0/go.mod h1:iBbtSCu2XBx23ZKBPSOrRkjjQPZFPuis4dIYUhu/chs=
-golang.org/x/mod v0.21.0 h1:vvrHzRwRfVKSiLrG+d4FMl/Qi4ukBCE6kZlTUkDYRT0=
-golang.org/x/mod v0.21.0/go.mod h1:6SkKJ3Xj0I0BrPOZoBy3bdMptDDU9oJrpohJ3eWZ1fY=
 golang.org/x/mod v0.24.0 h1:ZfthKaKaT4NrhGVZHO1/WDTwGES4De8KtWO0SIbNJMU=
 golang.org/x/mod v0.24.0/go.mod h1:IXM97Txy2VM4PJ3gI61r1YEk/gAj6zAHN3AdZt6S9Ww=
 golang.org/x/net v0.0.0-20180418062111-d41e8174641f/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
@@ -1432,8 +1430,6 @@ golang.org/x/net v0.6.0/go.mod h1:2Tu9+aMcznHK/AK1HMvgo6xiTLG5rD5rZLDS+rp2Bjs=
 golang.org/x/net v0.7.0/go.mod h1:2Tu9+aMcznHK/AK1HMvgo6xiTLG5rD5rZLDS+rp2Bjs=
 golang.org/x/net v0.8.0/go.mod h1:QVkue5JL9kW//ek3r6jTKnTFis1tRmNAW2P1shuFdJc=
 golang.org/x/net v0.10.0/go.mod h1:0qNGK6F8kojg2nk9dLZ2mShWaEBan6FAoqfSigmmuDg=
-golang.org/x/net v0.33.0 h1:74SYHlV8BIgHIFC/LrYkOGIwL19eTYXQ5wc6TBuO36I=
-golang.org/x/net v0.33.0/go.mod h1:HXLR5J+9DxmrqMwG9qjGCxZ+zKXxBru04zlTvWlWuN4=
 golang.org/x/net v0.40.0 h1:79Xs7wF06Gbdcg4kdCCIQArK11Z1hr5POQ6+fIYHNuY=
 golang.org/x/net v0.40.0/go.mod h1:y0hY0exeL2Pku80/zKK7tpntoX23cqL3Oa6njdgRtds=
 golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be/go.mod h1:N/0e6XlmueqKjAGxoOufVs8QHGRruUQn6yWY3a++T0U=
@@ -1469,8 +1465,6 @@ golang.org/x/sync v0.0.0-20201207232520-09787c993a3a/go.mod h1:RxMgew5VJxzue5/jJ
 golang.org/x/sync v0.0.0-20210220032951-036812b2e83c/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
 golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
 golang.org/x/sync v0.1.0/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
-golang.org/x/sync v0.11.0 h1:GGz8+XQP4FvTTrjZPzNKTMFtSXH80RAzG+5ghFPgK9w=
-golang.org/x/sync v0.11.0/go.mod h1:Czt+wKu1gCyEFDUtn0jG5QVvpJ6rzVqr5aXyt9drQfk=
 golang.org/x/sync v0.14.0 h1:woo0S4Yywslg6hp4eUFjTVOyKt0RookbpAHG4c1HmhQ=
 golang.org/x/sync v0.14.0/go.mod h1:1dzgHSNfp02xaA81J2MS99Qcpr2w7fw1gpm99rleRqA=
 golang.org/x/sys v0.0.0-20180418180225-9b0060990840/go.mod h1:STP8DvDyc/dI5b8T5hshtkjS+E42TnysNCUPdjciGhY=
@@ -1579,8 +1573,6 @@ golang.org/x/sys v0.7.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.8.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.12.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.20.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
-golang.org/x/sys v0.28.0 h1:Fksou7UEQUWlKvIdsqzJmUmCX3cZuD2+P3XyyzwMhlA=
-golang.org/x/sys v0.28.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
 golang.org/x/sys v0.33.0 h1:q3i8TbbEz+JRD9ywIRlyRAQbM0qF7hu24q3teo2hbuw=
 golang.org/x/sys v0.33.0/go.mod h1:BJP2sWEmIv4KK5OTEluFJCKSidICx8ciO85XgH3Ak8k=
 golang.org/x/term v0.0.0-20201117132131-f5c789dd3221/go.mod h1:Nr5EML6q2oocZ2LXRh80K7BxOlk5/8JxuGnuhpl+muw=
@@ -1592,8 +1584,6 @@ golang.org/x/term v0.5.0/go.mod h1:jMB1sMXY+tzblOD4FWmEbocvup2/aLOaQEp7JmGp78k=
 golang.org/x/term v0.6.0/go.mod h1:m6U89DPEgQRMq3DNkDClhWw02AUbt2daBVO4cn4Hv9U=
 golang.org/x/term v0.8.0/go.mod h1:xPskH00ivmX89bAKVGSKKtLOWNx2+17Eiy94tnKShWo=
 golang.org/x/term v0.20.0/go.mod h1:8UkIAJTvZgivsXaD6/pH6U9ecQzZ45awqEOzuCvwpFY=
-golang.org/x/term v0.27.0 h1:WP60Sv1nlK1T6SupCHbXzSaN0b9wUmsPoRS9b61A23Q=
-golang.org/x/term v0.27.0/go.mod h1:iMsnZpn0cago0GOrHO2+Y7u7JPn5AylBrcoWkElMTSM=
 golang.org/x/term v0.32.0 h1:DR4lr0TjUs3epypdhTOkMmuF5CDFJ/8pOnbzMZPQ7bg=
 golang.org/x/term v0.32.0/go.mod h1:uZG1FhGx848Sqfsq4/DlJr3xGGsYMu/L5GW4abiaEPQ=
 golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
@@ -1610,8 +1600,6 @@ golang.org/x/text v0.6.0/go.mod h1:mrYo+phRRbMaCq/xk9113O4dZlRixOauAjOtrjsXDZ8=
 golang.org/x/text v0.7.0/go.mod h1:mrYo+phRRbMaCq/xk9113O4dZlRixOauAjOtrjsXDZ8=
 golang.org/x/text v0.8.0/go.mod h1:e1OnstbJyHTd6l/uOt8jFFHp6TRDWZR/bV3emEE/zU8=
 golang.org/x/text v0.9.0/go.mod h1:e1OnstbJyHTd6l/uOt8jFFHp6TRDWZR/bV3emEE/zU8=
-golang.org/x/text v0.21.0 h1:zyQAAkrwaneQ066sspRyJaG9VNi/YJ1NfzcGB3hZ/qo=
-golang.org/x/text v0.21.0/go.mod h1:4IBbMaMmOPCJ8SecivzSH54+73PCFmPWxNTLm+vZkEQ=
 golang.org/x/text v0.25.0 h1:qVyWApTSYLk/drJRO5mDlNYskwQznZmkpV2c8q9zls4=
 golang.org/x/text v0.25.0/go.mod h1:WEdwpYrmk1qmdHvhkSTNPm3app7v4rsT8F2UD6+VHIA=
 golang.org/x/time v0.0.0-20181108054448-85acf8d2951c/go.mod h1:tRJNPiyCQ0inRvYxbN9jk5I+vvW/OXSQhTDSoE431IQ=
@@ -1691,8 +1679,6 @@ golang.org/x/tools v0.1.4/go.mod h1:o0xws9oXOQQZyjljx8fwUC0k7L1pTE6eaCbjGeHmOkk=
 golang.org/x/tools v0.1.5/go.mod h1:o0xws9oXOQQZyjljx8fwUC0k7L1pTE6eaCbjGeHmOkk=
 golang.org/x/tools v0.1.12/go.mod h1:hNGJHUnrk76NpqgfD5Aqm5Crs+Hm0VOH/i9J2+nxYbc=
 golang.org/x/tools v0.6.0/go.mod h1:Xwgl3UAJ/d3gWutnCtw505GrjyAbvKui8lOU390QaIU=
-golang.org/x/tools v0.25.0 h1:oFU9pkj/iJgs+0DT+VMHrx+oBKs/LJMV+Uvg78sl+fE=
-golang.org/x/tools v0.25.0/go.mod h1:/vtpO8WL1N9cQC3FN5zPqb//fRXskFHbLKk4OW1Q7rg=
 golang.org/x/tools v0.33.0 h1:4qz2S3zmRxbGIhDIAgjxvFutSvH5EfnsYrRBj0UI0bc=
 golang.org/x/tools v0.33.0/go.mod h1:CIJMaWEY88juyUfo7UbgPqbC8rU2OqfAV1h2Qp0oMYI=
 golang.org/x/xerrors v0.0.0-20190410155217-1f06c39b4373/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

ikuzo/domain/organization.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"slices"
 	"unicode"
 )
 
@@ -100,10 +101,8 @@ func (id OrganizationID) Valid() error {
 		return fmt.Errorf("minimum length = %d; %w", MinLengthID, ErrIDTooShort)
 	}
 
-	for _, p := range protectedOrgIDs {
-		if id == p {
-			return ErrIDExists
-		}
+	if slices.Contains(protectedOrgIDs, id) {
+		return ErrIDExists
 	}
 
 	for _, r := range id {

ikuzo/driver/elasticsearch/internal/mapping/v2.go
@@ -52,8 +52,28 @@ var v2Mapping = `{
 		}
 	},
 	"mappings":{
-			"dynamic": "strict",
+			"dynamic": true,
 			"date_detection" : false,
+			"_source": {
+				"excludes": ["fields"]
+			},
+			"dynamic_templates": [
+				{
+					"fields_as_multi_field": {
+						"path_match": "fields.*",
+						"mapping": {
+							"type": "text",
+							"fields": {
+								"keyword": {
+									"type": "keyword",
+									"ignore_above": 256,
+									"doc_values": true
+								}
+							}
+						}
+					}
+				}
+			],
 			"properties": {
 				"_checksum": {"type": "keyword"},
 				"meta": {
@@ -134,6 +154,9 @@ var v2Mapping = `{
 				},
 				"recordType": {"type": "short"},
 				"full_text": {"type": "text"},
+				"fields": {
+					"type": "object"
+				},
 				"resources": {
 					"type": "nested",
 					"properties": {
@@ -203,6 +226,26 @@ func V2MappingUpdate() string {
 // but will lead to index errors when these fields are not present due to the
 // 'strict' on dynamic creating of new fields in the index.
 var v2MappingUpdate = `{
+  "_source": {
+    "excludes": ["fields"]
+  },
+  "dynamic_templates": [
+    {
+      "fields_as_multi_field": {
+        "path_match": "fields.*",
+        "mapping": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256,
+              "doc_values": true
+            }
+          }
+        }
+      }
+    }
+  ],
   "properties": {
 	"_checksum": {"type": "keyword"},
 	"meta": {
@@ -214,6 +257,9 @@ var v2MappingUpdate = `{
 			"recDefID": {"type": "keyword"},
 			"aboutTypeURI": {"type": "keyword"}
 		}
+	},
+    "fields": {
+		"type": "object"
 	},
     "tree": {
       "properties": {

ikuzo/rdf/index/entry.go
@@ -293,8 +293,8 @@ func splitPeriod(c rune) bool {
 	return !unicode.IsNumber(c) && c != '-'
 }
 
-// MarshalJSON is a custom JSON marshaler for Entry that adds the SearchLabel as a field with Value as its value.
-// This allows for dynamic field mapping in search engines where the SearchLabel becomes a key in the JSON.
+// MarshalJSON is a custom JSON marshaler for Entry.
+// Previously this added the SearchLabel as a dynamic field, but that functionality has been removed.
 func (e *Entry) MarshalJSON() ([]byte, error) {
 	// Create a copy of the entry to avoid recursive marshaling
 	type EntryAlias Entry
@@ -304,65 +304,17 @@ func (e *Entry) MarshalJSON() ([]byte, error) {
 		EntryAlias: (*EntryAlias)(e),
 	}
 
-	// Marshal the alias entry to a map
-	data, err := json.Marshal(aliasEntry)
-	if err != nil {
-		return nil, err
-	}
-
-	// Only add the dynamic field if Entry is a Literal and has a value and search label
-	if e.EntryType == Literal && e.Value != "" && e.SearchLabel != "" {
-		// Parse the JSON into a map
-		var entryMap map[string]interface{}
-		if err := json.Unmarshal(data, &entryMap); err != nil {
-			return nil, err
-		}
-
-		// Add the SearchLabel as a field with Value as its value
-		entryMap[e.SearchLabel] = e.Value
-
-		// Marshal the map back to JSON
-		return json.Marshal(entryMap)
-	}
-
-	return data, nil
+	// Marshal the alias entry directly
+	return json.Marshal(aliasEntry)
 }
 
-// UnmarshalJSON is a custom JSON unmarshaler for Entry that checks for dynamic fields.
+// UnmarshalJSON is a custom JSON unmarshaler for Entry.
+// Previously this handled dynamic fields, but that functionality has been removed.
 func (e *Entry) UnmarshalJSON(data []byte) error {
-	// Create a map to unmarshal the JSON data
-	var entryMap map[string]interface{}
-	if err := json.Unmarshal(data, &entryMap); err != nil {
-		return err
-	}
-
-	// Create a copy of the map without the potential dynamic field
-	cleanMap := make(map[string]interface{})
-	for k, v := range entryMap {
-		cleanMap[k] = v
-	}
-
-	// Get SearchLabel field
-	if sl, ok := entryMap["searchLabel"]; ok && sl != nil {
-		if slStr, ok := sl.(string); ok {
-			// Check if the SearchLabel is also used as a dynamic field
-			if _, exists := entryMap[slStr]; exists {
-				// Remove from the clean map to avoid double unmarshaling
-				delete(cleanMap, slStr)
-			}
-		}
-	}
-
-	// Marshal the clean map back to JSON
-	cleanData, err := json.Marshal(cleanMap)
-	if err != nil {
-		return err
-	}
-
-	// Unmarshal to an alias to avoid recursive unmarshaling
+	// Unmarshal directly to an alias to avoid recursive unmarshaling
 	type EntryAlias Entry
 	aliasEntry := (*EntryAlias)(e)
-	if err := json.Unmarshal(cleanData, aliasEntry); err != nil {
+	if err := json.Unmarshal(data, aliasEntry); err != nil {
 		return err
 	}
 

ikuzo/rdf/index/entry_test.go
@@ -250,11 +250,11 @@ func TestEntry_MarshalJSON(t *testing.T) {
 				Value:       "Test Title",
 				EntryType:   Literal,
 			},
-			wantFields: []string{"dc_title", "searchLabel", "@value"},
+			wantFields: []string{"searchLabel", "@value"},
 			wantErr:    false,
 		},
 		{
-			name: "resource without dynamic field",
+			name: "resource with search label",
 			entry: &Entry{
 				ID:          "http://example.org/resource/1",
 				Predicate:   "http://purl.org/dc/elements/1.1/relation",
@@ -294,11 +294,11 @@ func TestEntry_MarshalJSON(t *testing.T) {
 				}
 			}
 
-			// If this is a test for dynamic field addition, verify the dynamic field was added
+			// Dynamic fields should NOT be included (as that functionality has been removed)
 			if tt.entry.EntryType == Literal && tt.entry.SearchLabel != "" && tt.entry.Value != "" {
-				expectedDynamicField := fmt.Sprintf(`"%s":"%s"`, tt.entry.SearchLabel, tt.entry.Value)
-				if !strings.Contains(gotStr, expectedDynamicField) {
-					t.Errorf("Entry.MarshalJSON() = %v, missing dynamic field %q", gotStr, expectedDynamicField)
+				unexpectedDynamicField := fmt.Sprintf(`"%s":"%s"`, tt.entry.SearchLabel, tt.entry.Value)
+				if strings.Contains(gotStr, unexpectedDynamicField) {
+					t.Errorf("Entry.MarshalJSON() = %v, contains unexpected dynamic field %q that should have been removed", gotStr, unexpectedDynamicField)
 				}
 			}
 

commit 8675a05987a6985b820c344f41b11aa6b32d71c6

test(rdf/index,namespace): align stale tests with intentional changes
rdf/index (all four bisect-verified, no production regression):
- TestAddContextLevels and TestGraph_IndexMessageWithFields were
  committed already-failing in 59f12715: context refs live on target
  resources, and Fields is keyed by searchLabel (what the ES dynamic
  template and semantic API consume), not predicate URI
- TestGraph_ContextLevelsInResources codified the inverted was-created
  branch that ef6dda0a deliberately fixed; assert Context on the
  level1/level2 targets instead of root GraphExternalContext
- TestGraph_Query/Path_with_filter: ef6dda0a keeps the _: prefix on
  blank-node IDs

namespace:
- d94611d4 added the wikidatawiki default (2207 -> 2208)
- 8416367a moved the preferred schema prefix to https://schema.org/;
  assert the new intent and the extra suspect duplicate (109 -> 110)

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

ikuzo/rdf/index/graph_index_test.go
@@ -167,11 +167,11 @@ func TestAddContextLevels(t *testing.T) {
 	is.NoErr(err)
 	is.True(len(jsonBytes) > 0)
 
-	is.True(len(g.Resources[0].Context) > 0)
-
-	// This verifies the basic functionality of addContextLevels
-	// The actual context references might not be set in the test environment
-	// but the flag is correctly updated
+	// Context refs are appended to the target (nested) resources; the
+	// root resource has no inbound context by definition.
+	is.Equal(len(g.Resources[0].Context), 0)
+	is.True(len(g.Resources[1].Context) > 0)
+	is.True(len(g.Resources[2].Context) > 0)
 }
 
 // TestGraph_ContextLevelsInResources tests that context levels are properly set in Resources
@@ -238,16 +238,14 @@ func TestGraph_ContextLevelsInResources(t *testing.T) {
 	// Verify contextIsSet is now true
 	is.Equal(g.contextIsSet, true)
 
-	// Based on our debugging, we now understand that this implementation stores
-	// context references in the GraphExternalContext field of the resource that
-	// "owns" the relation, rather than in the Context field of the target resource
-
-	// Verify that the root resource has a GraphExternalContext entry for level1
-	is.True(len(rootResource.GraphExternalContext) > 0)
+	// Context references are stored in the Context field of the target
+	// resource; GraphExternalContext is reserved for references to
+	// resources outside the graph.
+	is.Equal(len(rootResource.GraphExternalContext), 0)
 
 	// Find and validate the context reference for level1
 	foundLevel1Context := false
-	for _, ctx := range rootResource.GraphExternalContext {
+	for _, ctx := range level1Resource.Context {
 		if ctx.ObjectID == "urn:test:level1" && ctx.Predicate == "http://example.org/ontology/hasLevel1" {
 			foundLevel1Context = true
 			is.Equal(ctx.Subject, "urn:test:root")
@@ -257,6 +255,17 @@ func TestGraph_ContextLevelsInResources(t *testing.T) {
 		}
 	}
 	is.True(foundLevel1Context)
+
+	// The nested level2 resource gets a level-2 context ref from level1
+	foundLevel2Context := false
+	for _, ctx := range level2Resource.Context {
+		if ctx.ObjectID == "urn:test:level2" && ctx.Predicate == "http://example.org/ontology/hasLevel2" {
+			foundLevel2Context = true
+			is.Equal(ctx.Subject, "urn:test:level1")
+			is.Equal(ctx.Level, int32(2))
+		}
+	}
+	is.True(foundLevel2Context)
 
 	// Test that the context flag is set correctly
 	// This confirms that the addContextLevels function was called and completed successfully
@@ -324,20 +333,21 @@ func TestGraph_IndexMessageWithFields(t *testing.T) {
 	fields, ok := graphData["fields"].(map[string]interface{})
 	is.True(ok) // Fields should be in the JSON
 
-	// Check that the fields contain both predicates with correct values
-	titleValues, ok := fields["http://purl.org/dc/elements/1.1/title"].([]interface{})
+	// Fields are keyed by SearchLabel, not predicate URI: the ES dynamic
+	// template maps fields.* and the semantic API selects fields by label.
+	titleValues, ok := fields["dc_title"].([]interface{})
 	is.True(ok)
 	is.Equal(len(titleValues), 2) // Should have both title values
 
 	// Check that dc:date has one value
-	dateValues, ok := fields["http://purl.org/dc/elements/1.1/date"].([]interface{})
+	dateValues, ok := fields["dc_date"].([]interface{})
 	is.True(ok)
 	is.Equal(len(dateValues), 1)
 	is.Equal(dateValues[0], "2020-01-01")
 
 	// Verify that g.Fields was populated
 	is.True(g.Fields != nil)
-	is.Equal(len(g.Fields), 2) // Should have dc:title and dc:date
-	is.Equal(len(g.Fields["http://purl.org/dc/elements/1.1/title"]), 2)
-	is.Equal(len(g.Fields["http://purl.org/dc/elements/1.1/date"]), 1)
+	is.Equal(len(g.Fields), 2) // Should have dc_title and dc_date
+	is.Equal(len(g.Fields["dc_title"]), 2)
+	is.Equal(len(g.Fields["dc_date"]), 1)
 }

ikuzo/rdf/index/graph_query_test.go
@@ -48,7 +48,7 @@ func TestGraph_Query(t *testing.T) {
 			wantResources:   1,
 			wantEntries:     0,
 			wantErr:         false,
-			checkResourceID: "b9b5bf0b4a85e3c",
+			checkResourceID: "_:b9b5bf0b4a85e3c",
 		},
 		{
 			name:            "Path with filter and value",

ikuzo/service/x/namespace/service_test.go
@@ -21,7 +21,7 @@ import (
 	"github.com/matryer/is"
 )
 
-const defaultListSize = 2207
+const defaultListSize = 2208
 
 func TestService_SearchLabel(t *testing.T) {
 	dc := &domain.Namespace{
@@ -246,17 +246,19 @@ func TestDefaults(t *testing.T) {
 	is.Equal(len(nsList), 1)
 	t.Logf("ns list schema: %#v", nsList)
 
+	// Since 8416367a the preferred schema prefix maps to https://schema.org/
+	// (Weight 100); http://schema.org/ keeps the demoted sdo/sorg prefixes
+	// with auto-assigned weights.
 	nsList, err = svc.List(&ListOptions{URI: "http://schema.org/"})
 	is.NoErr(err)
 	is.Equal(len(nsList), 2)
 	t.Logf("ns list schema from base: %#v", nsList)
-	is.Equal(nsList[len(nsList)-1].Weight, 3)
-	is.Equal(nsList[0].Weight, 100)
 
-	ns, err := svc.GetWithBase("http://schema.org/")
+	ns, err := svc.GetWithBase("https://schema.org/")
 	is.NoErr(err)
 	t.Logf("ns: %#v", ns)
 	is.Equal(ns.Prefix, "schema")
+	is.Equal(ns.Weight, 100)
 
 	ns, err = svc.GetWithPrefix("sdo")
 	is.NoErr(err)

ikuzo/service/x/namespace/store_test.go
@@ -174,5 +174,7 @@ func TestDuplicateURIS(t *testing.T) {
 
 	// TODO(kiivihal): finish this in default
 	t.Logf("suspect uris (%d): %#v", len(suspect), suspect)
-	is.Equal(len(suspect), 109)
+	// 110 since 8416367a demoted http://schema.org/ to the unweighted
+	// sdo/sorg prefixes, making it a suspect duplicate.
+	is.Equal(len(suspect), 110)
 }